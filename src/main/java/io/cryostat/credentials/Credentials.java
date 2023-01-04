1b1dd108c8f04916b1e71ec3c920cc10a1c0bd80
Andrew Azores <aazores@redhat.com>
2023-01-04 17:53:24 -0500

    flesh out JMX credentials API with encrypted keyring

--- src/main/java/io/cryostat/credentials/Credentials.java
+++ src/main/java/io/cryostat/credentials/Credentials.java
@@ -37,15 +37,26 @@
  */
 package io.cryostat.credentials;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import io.cryostat.V2Response;
+import io.cryostat.ws.MessagingServer;
+import io.cryostat.ws.Notification;
+
 import io.vertx.core.eventbus.EventBus;
+import org.jboss.resteasy.reactive.RestForm;
+import org.jboss.resteasy.reactive.RestPath;
 
 @Path("/api/v2.2/credentials")
 public class Credentials {
@@ -55,6 +66,66 @@ public class Credentials {
     @GET
     @RolesAllowed("credential:read")
     public V2Response list() {
-        return V2Response.json(List.of());
+        List<Credential> credentials = Credential.listAll();
+        return V2Response.json(credentials.stream().map(this::safeResult).toList());
+    }
+
+    @GET
+    @RolesAllowed("credential:read")
+    @Path("/{id}")
+    public V2Response get(@RestPath long id) {
+        Credential credential = Credential.findById(id);
+        if (credential == null) {
+            throw new NotFoundException();
+        }
+        return V2Response.json(safeMatchedResult(credential));
+    }
+
+    @Transactional
+    @POST
+    @RolesAllowed("credential:create")
+    public void create(
+            @RestForm String matchExpression,
+            @RestForm String username,
+            @RestForm String password) {
+        Credential credential = new Credential();
+        credential.matchExpression = matchExpression;
+        credential.username = username;
+        credential.password = password;
+        credential.persist();
+        bus.publish(
+                MessagingServer.class.getName(),
+                new Notification("CredentialsStored", safeResult(credential)));
+    }
+
+    @Transactional
+    @DELETE
+    @RolesAllowed("credential:delete")
+    @Path("/{id}")
+    public void delete(@RestPath long id) {
+        Credential credential = Credential.findById(id);
+        if (credential == null) {
+            throw new NotFoundException();
+        }
+        credential.delete();
+        bus.publish(
+                MessagingServer.class.getName(),
+                new Notification("CredentialsDeleted", safeResult(credential)));
+    }
+
+    private Map<String, Object> safeResult(Credential credential) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", credential.id);
+        result.put("matchExpression", credential.matchExpression);
+        // TODO
+        result.put("numMatchingTargets", 0);
+        return result;
+    }
+
+    private Map<String, Object> safeMatchedResult(Credential credential) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("matchExpression", credential.matchExpression);
+        result.put("targets", List.of());
+        return result;
     }
 }
