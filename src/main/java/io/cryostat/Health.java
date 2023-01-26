cryostatio/cryostat

commit 036f03923de5d8ee04534832bcd496b5ad893e66

tinkering with redirects and API root path

src/main/java/io/cryostat/credentials/Credentials.java
@@ -54,7 +54,7 @@ import io.cryostat.V2Response;
 import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.RestPath;
 
-@Path("/api/v2.2/credentials")
+@Path("v2.2/credentials")
 public class Credentials {
 
     @GET

src/main/java/io/cryostat/discovery/CustomDiscovery.java
@@ -49,7 +49,6 @@ import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import javax.persistence.NoResultException;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -70,6 +69,7 @@ import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.RestPath;
 import org.jboss.resteasy.reactive.RestQuery;
+import org.jboss.resteasy.reactive.RestResponse;
 
 @ApplicationScoped
 @Path("")
@@ -99,7 +99,7 @@ public class CustomDiscovery {
 
     @Transactional(rollbackOn = {JvmIdException.class})
     @POST
-    @Path("/api/v2/targets")
+    @Path("v2/targets")
     @Consumes("application/json")
     @RolesAllowed("write")
     public Response create(Target target, @RestQuery boolean dryrun) {
@@ -137,7 +137,7 @@ public class CustomDiscovery {
             node.persist();
             realm.persist();
 
-            return Response.created(URI.create("/api/v3/targets/" + target.id)).build();
+            return Response.created(URI.create("v3/targets/" + target.id)).build();
         } catch (Exception e) {
             if (ExceptionUtils.indexOfType(e, ConstraintViolationException.class) >= 0) {
                 logger.warn("Invalid target definition", e);
@@ -150,7 +150,7 @@ public class CustomDiscovery {
 
     @Transactional
     @POST
-    @Path("/api/v2/targets")
+    @Path("v2/targets")
     @Consumes("multipart/form-data")
     @RolesAllowed("write")
     public Response create(
@@ -164,23 +164,18 @@ public class CustomDiscovery {
 
     @Transactional
     @DELETE
-    @Path("/api/v2/targets/{connectUrl}")
+    @Path("v2/targets/{connectUrl}")
     @RolesAllowed("write")
     public Response delete(@RestPath URI connectUrl) throws URISyntaxException {
-        try {
-            Target target = Target.getTargetByConnectUrl(connectUrl);
-            return delete(target.id);
-        } catch (Exception e) {
-            if (ExceptionUtils.indexOfType(e, NoResultException.class) >= 0) {
-                return Response.status(404).build();
-            }
-            return Response.serverError().build();
-        }
+        Target target = Target.getTargetByConnectUrl(connectUrl);
+        return Response.status(RestResponse.Status.PERMANENT_REDIRECT)
+                .location(URI.create(String.format("v3/targets/%d", target.id)))
+                .build();
     }
 
     @Transactional
     @DELETE
-    @Path("/api/v3/targets/{id}")
+    @Path("v3/targets/{id}")
     @RolesAllowed("write")
     public Response delete(@RestPath long id) throws URISyntaxException {
         Target target = Target.findById(id);

src/main/java/io/cryostat/discovery/Discovery.java
@@ -56,6 +56,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import io.cryostat.discovery.DiscoveryPlugin.PluginCallback;
 import io.cryostat.targets.TargetConnectionManager;
@@ -67,6 +68,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.RestPath;
 import org.jboss.resteasy.reactive.RestQuery;
+import org.jboss.resteasy.reactive.RestResponse;
 
 @Path("")
 public class Discovery {
@@ -101,7 +103,16 @@ public class Discovery {
     }
 
     @GET
-    @Path("/api/v2.1/discovery")
+    @Path("v2.1/discovery")
+    @RolesAllowed("read")
+    public Response getv21() {
+        return Response.status(RestResponse.Status.MOVED_PERMANENTLY)
+                .location(URI.create("/api/v3/discovery"))
+                .build();
+    }
+
+    @GET
+    @Path("v3/discovery")
     @RolesAllowed("read")
     public DiscoveryNode get() {
         return DiscoveryNode.getUniverse();
@@ -109,7 +120,7 @@ public class Discovery {
 
     @Transactional
     @POST
-    @Path("/api/v2.2/discovery")
+    @Path("v2.2/discovery")
     @Consumes("application/json")
     @RolesAllowed("write")
     public Map<String, Object> register(JsonObject body) throws URISyntaxException {
@@ -146,7 +157,7 @@ public class Discovery {
 
     @Transactional
     @POST
-    @Path("/api/v2.2/discovery/{id}")
+    @Path("v2.2/discovery/{id}")
     @Consumes("application/json")
     @PermitAll
     public Map<String, Map<String, String>> publish(
@@ -170,7 +181,7 @@ public class Discovery {
 
     @Transactional
     @DELETE
-    @Path("/api/v2.2/discovery/{id}")
+    @Path("v2.2/discovery/{id}")
     @PermitAll
     public Map<String, Map<String, String>> deregister(@RestPath UUID id, @RestQuery String token) {
         DiscoveryPlugin plugin = DiscoveryPlugin.findById(id);
@@ -190,7 +201,7 @@ public class Discovery {
     }
 
     @GET
-    @Path("/api/v3/discovery_plugins")
+    @Path("v3/discovery_plugins")
     @RolesAllowed("read")
     public List<DiscoveryPlugin> getPlugins(@RestQuery String realm) {
         List<DiscoveryPlugin> plugins = DiscoveryPlugin.findAll().list();
@@ -200,7 +211,7 @@ public class Discovery {
     }
 
     @GET
-    @Path("/api/v3/discovery_plugins/{id}")
+    @Path("v3/discovery_plugins/{id}")
     @RolesAllowed("read")
     public DiscoveryPlugin getPlugin(@RestPath UUID id) {
         return DiscoveryPlugin.findById(id);

src/main/java/io/cryostat/events/EventTemplates.java
@@ -46,6 +46,7 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import io.cryostat.core.templates.Template;
 import io.cryostat.core.templates.TemplateType;
@@ -53,6 +54,7 @@ import io.cryostat.targets.Target;
 import io.cryostat.targets.TargetConnectionManager;
 
 import org.jboss.resteasy.reactive.RestPath;
+import org.jboss.resteasy.reactive.RestResponse;
 
 @Path("")
 public class EventTemplates {
@@ -69,15 +71,17 @@ public class EventTemplates {
     @Inject TargetConnectionManager connectionManager;
 
     @GET
-    @Path("/api/v1/targets/{connectUrl}/templates")
+    @Path("v1/targets/{connectUrl}/templates")
     @RolesAllowed("read")
-    public List<Template> listTemplatesV1(@RestPath URI connectUrl) throws Exception {
+    public Response listTemplatesV1(@RestPath URI connectUrl) throws Exception {
         Target target = Target.getTargetByConnectUrl(connectUrl);
-        return listTemplates(target.id);
+        return Response.status(RestResponse.Status.PERMANENT_REDIRECT)
+                .location(URI.create(String.format("v3/targets/%d/event_templates", target.id)))
+                .build();
     }
 
     @GET
-    @Path("/api/v3/targets/{id}/event_templates")
+    @Path("v3/targets/{id}/event_templates")
     @RolesAllowed("read")
     public List<Template> listTemplates(@RestPath long id) throws Exception {
         Target target = Target.findById(id);

src/main/java/io/cryostat/events/Events.java
@@ -44,11 +44,13 @@ import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import io.cryostat.targets.Target;
 import io.cryostat.targets.TargetConnectionManager;
 
 import org.jboss.resteasy.reactive.RestPath;
+import org.jboss.resteasy.reactive.RestResponse;
 
 @Path("")
 public class Events {
@@ -56,15 +58,17 @@ public class Events {
     @Inject TargetConnectionManager connectionManager;
 
     @GET
-    @Path("/api/v1/targets/{connectUrl}/events")
+    @Path("v1/targets/{connectUrl}/events")
     @RolesAllowed("read")
-    public List<SerializableEventTypeInfo> listEventsV1(@RestPath URI connectUrl) throws Exception {
+    public Response listEventsV1(@RestPath URI connectUrl) throws Exception {
         Target target = Target.getTargetByConnectUrl(connectUrl);
-        return listEvents(target.id);
+        return Response.status(RestResponse.Status.PERMANENT_REDIRECT)
+                .location(URI.create(String.format("v3/targets/%d/events", target.id)))
+                .build();
     }
 
     @GET
-    @Path("/api/v3/targets/{id}/events")
+    @Path("v3/targets/{id}/events")
     @RolesAllowed("read")
     public List<SerializableEventTypeInfo> listEvents(@RestPath long id) throws Exception {
         Target target = Target.findById(id);

commit e8455378cb70d6ed0f0500082843594723e53cb4

undo 036f03923de5d8ee04534832bcd496b5ad893e66, broke /health

src/main/java/io/cryostat/Health.java
@@ -49,7 +49,7 @@ import javax.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
-@Path("/")
+@Path("")
 class Health {
 
     @ConfigProperty(name = "quarkus.application.name")
@@ -73,7 +73,7 @@ class Health {
     @Inject Logger logger;
 
     @GET
-    @Path("health")
+    @Path("/health")
     @PermitAll
     public Response health() {
         return Response.ok(
@@ -104,12 +104,12 @@ class Health {
     }
 
     @GET
-    @Path("health/liveness")
+    @Path("/health/liveness")
     @PermitAll
     public void liveness() {}
 
     @GET
-    @Path("api/v1/notifications_url")
+    @Path("/api/v1/notifications_url")
     @PermitAll
     public Response notificationsUrl() {
         // TODO @PermitAll annotation seems to skip the CORS filter, so these headers don't get

src/main/java/io/cryostat/credentials/Credentials.java
@@ -45,7 +45,6 @@ import javax.annotation.security.RolesAllowed;
 import javax.transaction.Transactional;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
@@ -54,7 +53,7 @@ import io.cryostat.V2Response;
 import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.RestPath;
 
-@Path("v2.2/credentials")
+@Path("/api/v2.2/credentials")
 public class Credentials {
 
     @GET
@@ -68,10 +67,7 @@ public class Credentials {
     @RolesAllowed("read")
     @Path("/{id}")
     public V2Response get(@RestPath long id) {
-        Credential credential = Credential.findById(id);
-        if (credential == null) {
-            throw new NotFoundException();
-        }
+        Credential credential = Credential.find("id", id).singleResult();
         return V2Response.json(safeMatchedResult(credential));
     }
 
@@ -94,10 +90,7 @@ public class Credentials {
     @RolesAllowed("write")
     @Path("/{id}")
     public void delete(@RestPath long id) {
-        Credential credential = Credential.findById(id);
-        if (credential == null) {
-            throw new NotFoundException();
-        }
+        Credential credential = Credential.find("id", id).singleResult();
         credential.delete();
     }
 

src/main/java/io/cryostat/discovery/CustomDiscovery.java
@@ -169,7 +169,7 @@ public class CustomDiscovery {
     public Response delete(@RestPath URI connectUrl) throws URISyntaxException {
         Target target = Target.getTargetByConnectUrl(connectUrl);
         return Response.status(RestResponse.Status.PERMANENT_REDIRECT)
-                .location(URI.create(String.format("v3/targets/%d", target.id)))
+                .location(URI.create(String.format("/api/v3/targets/%d", target.id)))
                 .build();
     }
 
@@ -178,10 +178,7 @@ public class CustomDiscovery {
     @Path("v3/targets/{id}")
     @RolesAllowed("write")
     public Response delete(@RestPath long id) throws URISyntaxException {
-        Target target = Target.findById(id);
-        if (target == null) {
-            return Response.status(404).build();
-        }
+        Target target = Target.find("id", id).singleResult();
         DiscoveryNode realm = DiscoveryNode.getRealm(REALM).orElseThrow();
         realm.children.remove(target.discoveryNode);
         target.delete();

src/main/java/io/cryostat/discovery/Discovery.java
@@ -53,7 +53,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -109,16 +108,16 @@ public class Discovery {
     }
 
     @GET
-    @Path("v2.1/discovery")
+    @Path("/api/v2.1/discovery")
     @RolesAllowed("read")
     public Response getv21() {
-        return Response.status(RestResponse.Status.MOVED_PERMANENTLY)
-                .location(URI.create("v3/discovery"))
+        return Response.status(RestResponse.Status.PERMANENT_REDIRECT)
+                .location(URI.create("/api/v3/discovery"))
                 .build();
     }
 
     @GET
-    @Path("v3/discovery")
+    @Path("/api/v3/discovery")
     @RolesAllowed("read")
     public DiscoveryNode get() {
         return DiscoveryNode.getUniverse();
@@ -126,7 +125,7 @@ public class Discovery {
 
     @Transactional
     @POST
-    @Path("v2.2/discovery")
+    @Path("/api/v2.2/discovery")
     @Consumes("application/json")
     @RolesAllowed("write")
     public Map<String, Object> register(JsonObject body) throws URISyntaxException {
@@ -163,15 +162,12 @@ public class Discovery {
 
     @Transactional
     @POST
-    @Path("v2.2/discovery/{id}")
+    @Path("/api/v2.2/discovery/{id}")
     @Consumes("application/json")
     @PermitAll
     public Map<String, Map<String, String>> publish(
             @RestPath UUID id, @RestQuery String token, List<DiscoveryNode> body) {
-        DiscoveryPlugin plugin = DiscoveryPlugin.findById(id);
-        if (plugin == null) {
-            throw new NotFoundException();
-        }
+        DiscoveryPlugin plugin = DiscoveryPlugin.find("id", id).singleResult();
         plugin.realm.children.clear();
         plugin.realm.children.addAll(body);
         body.forEach(b -> b.persist());
@@ -187,13 +183,10 @@ public class Discovery {
 
     @Transactional
     @DELETE
-    @Path("v2.2/discovery/{id}")
+    @Path("/api/v2.2/discovery/{id}")
     @PermitAll
     public Map<String, Map<String, String>> deregister(@RestPath UUID id, @RestQuery String token) {
-        DiscoveryPlugin plugin = DiscoveryPlugin.findById(id);
-        if (plugin == null) {
-            throw new NotFoundException();
-        }
+        DiscoveryPlugin plugin = DiscoveryPlugin.find("id", id).singleResult();
         if (plugin.builtin) {
             throw new ForbiddenException();
         }
@@ -207,7 +200,7 @@ public class Discovery {
     }
 
     @GET
-    @Path("v3/discovery_plugins")
+    @Path("/api/v3/discovery_plugins")
     @RolesAllowed("read")
     public List<DiscoveryPlugin> getPlugins(@RestQuery String realm)
             throws JsonProcessingException {
@@ -219,7 +212,7 @@ public class Discovery {
     }
 
     @GET
-    @Path("v3/discovery_plugins/{id}")
+    @Path("/api/v3/discovery_plugins/{id}")
     @RolesAllowed("read")
     public DiscoveryPlugin getPlugin(@RestPath UUID id) throws JsonProcessingException {
         return DiscoveryPlugin.find("id", id).singleResult();

src/main/java/io/cryostat/events/EventTemplates.java
@@ -44,7 +44,6 @@ import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
@@ -71,23 +70,21 @@ public class EventTemplates {
     @Inject TargetConnectionManager connectionManager;
 
     @GET
-    @Path("v1/targets/{connectUrl}/templates")
+    @Path("/api/v1/targets/{connectUrl}/templates")
     @RolesAllowed("read")
     public Response listTemplatesV1(@RestPath URI connectUrl) throws Exception {
         Target target = Target.getTargetByConnectUrl(connectUrl);
         return Response.status(RestResponse.Status.PERMANENT_REDIRECT)
-                .location(URI.create(String.format("v3/targets/%d/event_templates", target.id)))
+                .location(
+                        URI.create(String.format("/api/v3/targets/%d/event_templates", target.id)))
                 .build();
     }
 
     @GET
-    @Path("v3/targets/{id}/event_templates")
+    @Path("/api/v3/targets/{id}/event_templates")
     @RolesAllowed("read")
     public List<Template> listTemplates(@RestPath long id) throws Exception {
-        Target target = Target.findById(id);
-        if (target == null) {
-            throw new NotFoundException();
-        }
+        Target target = Target.find("id", id).singleResult();
         return connectionManager.executeConnectedTask(
                 target,
                 connection -> {

src/main/java/io/cryostat/events/Events.java
@@ -58,20 +58,20 @@ public class Events {
     @Inject TargetConnectionManager connectionManager;
 
     @GET
-    @Path("v1/targets/{connectUrl}/events")
+    @Path("/api/v1/targets/{connectUrl}/events")
     @RolesAllowed("read")
     public Response listEventsV1(@RestPath URI connectUrl) throws Exception {
         Target target = Target.getTargetByConnectUrl(connectUrl);
         return Response.status(RestResponse.Status.PERMANENT_REDIRECT)
-                .location(URI.create(String.format("v3/targets/%d/events", target.id)))
+                .location(URI.create(String.format("/api/v3/targets/%d/events", target.id)))
                 .build();
     }
 
     @GET
-    @Path("v3/targets/{id}/events")
+    @Path("/api/v3/targets/{id}/events")
     @RolesAllowed("read")
     public List<SerializableEventTypeInfo> listEvents(@RestPath long id) throws Exception {
-        Target target = Target.findById(id);
+        Target target = Target.find("id", id).singleResult();
         return connectionManager.executeConnectedTask(
                 target,
                 connection ->

src/main/java/io/cryostat/recordings/Recordings.java
@@ -154,7 +154,7 @@ public class Recordings {
     }
 
     @GET
-    @Path("v1/recordings")
+    @Path("/api/v1/recordings")
     @RolesAllowed("read")
     public List<ArchivedRecording> listArchivesV1() {
         var result = new ArrayList<ArchivedRecording>();
@@ -200,7 +200,7 @@ public class Recordings {
     }
 
     @POST
-    @Path("v1/recordings")
+    @Path("/api/v1/recordings")
     @RolesAllowed("write")
     public Map<String, Object> upload(
             @RestForm("recording") FileUpload recording, @RestForm("labels") JsonObject rawLabels)
@@ -214,7 +214,7 @@ public class Recordings {
     }
 
     @POST
-    @Path("beta/recordings/{jvmId}")
+    @Path("/api/beta/recordings/{jvmId}")
     @RolesAllowed("write")
     public void agentPush(
             @RestPath String jvmId,
@@ -313,7 +313,7 @@ public class Recordings {
 
     @GET
     @Blocking
-    @Path("beta/recordings/{jvmId}")
+    @Path("/api/beta/recordings/{jvmId}")
     @RolesAllowed("read")
     public List<ArchivedRecording> agentGet(@RestPath String jvmId) {
         var result = new ArrayList<ArchivedRecording>();
@@ -362,7 +362,7 @@ public class Recordings {
     }
 
     @DELETE
-    @Path("beta/recordings/{jvmId}/{filename}")
+    @Path("/api/beta/recordings/{jvmId}/{filename}")
     @RolesAllowed("write")
     public void agentDelete(
             @RestPath String jvmId,
@@ -439,7 +439,7 @@ public class Recordings {
     }
 
     @DELETE
-    @Path("v1/recordings/{filename}")
+    @Path("/api/v1/recordings/{filename}")
     @RolesAllowed("write")
     @Blocking
     public void delete(@RestPath String filename) throws Exception {
@@ -451,7 +451,7 @@ public class Recordings {
     }
 
     @GET
-    @Path("beta/fs/recordings")
+    @Path("/api/beta/fs/recordings")
     @RolesAllowed("read")
     public Collection<ArchivedRecordingDirectory> listFsArchives() {
         var map = new HashMap<String, ArchivedRecordingDirectory>();
@@ -516,18 +516,15 @@ public class Recordings {
     }
 
     @GET
-    @Path("v3/targets/{id}/recordings")
+    @Path("/api/v3/targets/{id}/recordings")
     @RolesAllowed("read")
     public List<LinkedRecordingDescriptor> listForTarget(@RestPath long id) throws Exception {
-        Target target = Target.findById(id);
-        if (target == null) {
-            throw new NotFoundException();
-        }
+        Target target = Target.find("id", id).singleResult();
         return target.activeRecordings.stream().map(ActiveRecording::toExternalForm).toList();
     }
 
     @GET
-    @Path("v1/targets/{connectUrl}/recordings")
+    @Path("/api/v1/targets/{connectUrl}/recordings")
     @RolesAllowed("read")
     public List<LinkedRecordingDescriptor> listForTargetByUrl(@RestPath URI connectUrl)
             throws Exception {
@@ -537,14 +534,11 @@ public class Recordings {
 
     @PATCH
     @Transactional
-    @Path("v3/targets/{targetId}/recordings/{remoteId}")
+    @Path("/api/v3/targets/{targetId}/recordings/{remoteId}")
     @RolesAllowed("write")
     public String patch(@RestPath long targetId, @RestPath long remoteId, String body)
             throws Exception {
-        Target target = Target.findById(targetId);
-        if (target == null) {
-            throw new NotFoundException();
-        }
+        Target target = Target.find("id", targetId).singleResult();
         Optional<ActiveRecording> recording =
                 target.activeRecordings.stream()
                         .filter(rec -> rec.remoteId == remoteId)
@@ -607,7 +601,7 @@ public class Recordings {
 
     @PATCH
     @Transactional
-    @Path("v1/targets/{connectUrl}/recordings/{recordingName}")
+    @Path("/api/v1/targets/{connectUrl}/recordings/{recordingName}")
     @RolesAllowed("write")
     public String patchV1(@RestPath URI connectUrl, @RestPath String recordingName, String body)
             throws Exception {
@@ -623,7 +617,7 @@ public class Recordings {
 
     @Transactional
     @POST
-    @Path("v3/targets/{id}/recordings")
+    @Path("/api/v3/targets/{id}/recordings")
     @RolesAllowed("write")
     public LinkedRecordingDescriptor createRecording(
             @RestPath long id,
@@ -643,7 +637,7 @@ public class Recordings {
             throw new BadRequestException("\"events\" form parameter must be provided");
         }
 
-        Target target = Target.findById(id);
+        Target target = Target.find("id", id).singleResult();
         LinkedRecordingDescriptor descriptor =
                 connectionManager.executeConnectedTask(
                         target,
@@ -739,11 +733,11 @@ public class Recordings {
     @Transactional
     void stopRecording(long targetId, long recordingId, boolean archive) {
         try {
-            ActiveRecording recording = ActiveRecording.findById(recordingId);
+            ActiveRecording recording = ActiveRecording.find("id", recordingId).singleResult();
             recording.state = RecordingState.STOPPED;
             recording.persist();
             if (archive) {
-                saveRecording(Target.findById(targetId), recording);
+                saveRecording(Target.find("id", targetId).singleResult(), recording);
             }
         } catch (Exception e) {
             logger.error("couldn't update recording", e);
@@ -779,7 +773,7 @@ public class Recordings {
 
     @Transactional
     @POST
-    @Path("v1/targets/{connectUrl}/recordings")
+    @Path("/api/v1/targets/{connectUrl}/recordings")
     @RolesAllowed("write")
     public LinkedRecordingDescriptor createRecordingV1(
             @RestPath URI connectUrl,
@@ -807,7 +801,7 @@ public class Recordings {
 
     @Transactional
     @DELETE
-    @Path("v1/targets/{connectUrl}/recordings/{recordingName}")
+    @Path("/api/v1/targets/{connectUrl}/recordings/{recordingName}")
     @RolesAllowed("write")
     public void deleteRecordingV1(@RestPath URI connectUrl, @RestPath String recordingName)
             throws Exception {
@@ -827,10 +821,10 @@ public class Recordings {
 
     @Transactional
     @DELETE
-    @Path("v3/targets/{targetId}/recordings/{remoteId}")
+    @Path("/api/v3/targets/{targetId}/recordings/{remoteId}")
     @RolesAllowed("write")
     public void deleteRecording(@RestPath long targetId, @RestPath long remoteId) throws Exception {
-        Target target = Target.findById(targetId);
+        Target target = Target.find("id", targetId).singleResult();
         target.activeRecordings.stream()
                 .filter(r -> r.remoteId == remoteId)
                 .findFirst()
@@ -843,7 +837,7 @@ public class Recordings {
 
     @Blocking
     @DELETE
-    @Path("beta/fs/recordings/{jvmId}/{filename}")
+    @Path("/api/beta/fs/recordings/{jvmId}/{filename}")
     @RolesAllowed("write")
     public void deleteArchivedRecording(@RestPath String jvmId, @RestPath String filename)
             throws Exception {
@@ -883,7 +877,7 @@ public class Recordings {
     }
 
     @GET
-    @Path("v1/targets/{connectUrl}/recordingOptions")
+    @Path("/api/v1/targets/{connectUrl}/recordingOptions")
     @RolesAllowed("read")
     public Map<String, Object> getRecordingOptionsV1(@RestPath URI connectUrl) throws Exception {
         Target target = Target.getTargetByConnectUrl(connectUrl);
@@ -891,10 +885,10 @@ public class Recordings {
     }
 
     @GET
-    @Path("v3/targets/{id}/recordingOptions")
+    @Path("/api/v3/targets/{id}/recordingOptions")
     @RolesAllowed("read")
     public Map<String, Object> getRecordingOptions(@RestPath long id) throws Exception {
-        Target target = Target.findById(id);
+        Target target = Target.find("id", id).singleResult();
         return connectionManager.executeConnectedTask(
                 target,
                 connection -> {

src/main/java/io/cryostat/rules/Rules.java
@@ -55,7 +55,7 @@ import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.RestPath;
 import org.jboss.resteasy.reactive.RestQuery;
 
-@Path("v2/rules")
+@Path("/api/v2/rules")
 public class Rules {
 
     @Inject EventBus bus;

src/main/java/io/cryostat/security/Auth.java
@@ -58,11 +58,11 @@ public class Auth {
     @Inject Logger logger;
 
     @POST
-    @Path("v2.1/logout")
+    @Path("/api/v2.1/logout")
     public void logout() {}
 
     @POST
-    @Path("v2.1/auth")
+    @Path("/api/v2.1/auth")
     @PermitAll
     @Produces("application/json")
     public Response login(@Context RoutingContext context) {

src/main/java/io/cryostat/targets/Targets.java
@@ -42,7 +42,6 @@ import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
@@ -53,29 +52,25 @@ import org.jboss.resteasy.reactive.RestResponse;
 public class Targets {
 
     @GET
-    @Path("v1/targets")
+    @Path("/api/v1/targets")
     @RolesAllowed("read")
     public Response listV1() {
         return Response.status(RestResponse.Status.PERMANENT_REDIRECT)
-                .location(URI.create("v3/targets"))
+                .location(URI.create("/api/v3/targets"))
                 .build();
     }
 
     @GET
-    @Path("v3/targets")
+    @Path("/api/v3/targets")
     @RolesAllowed("read")
     public List<Target> list() {
         return Target.listAll();
     }
 
     @GET
-    @Path("v3/targets/{id}")
+    @Path("/api/v3/targets/{id}")
     @RolesAllowed("read")
     public Target getById(@RestPath Long id) {
-        Target target = Target.findById(id);
-        if (target == null) {
-            throw new NotFoundException();
-        }
-        return target;
+        return Target.find("id", id).singleResult();
     }
 }

src/main/resources/application.properties
@@ -5,7 +5,6 @@ cryostat.jdp.enabled=false
 # the console URL and access credentials can also be found here while in dev mode: http://localhost:8181/q/dev/
 minio.buckets.archives.name=archivedrecordings
 
-quarkus.resteasy-reactive.path=/api/
 quarkus.http.auth.proactive=false
 quarkus.http.host=0.0.0.0
 %dev.quarkus.http.host=localhost