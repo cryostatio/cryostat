4437211ea40faebc11a79945e581c99bc6e6254e
Andrew Azores <aazores@redhat.com>
2023-04-28 14:54:03 -0400

    always pull base image

--- db/build.sh
+++ db/build.sh
@@ -2,5 +2,5 @@
 
 DIR="$(dirname "$(readlink -f "$0")")"
 
-podman build -t quay.io/cryostat/cryostat3-db:latest -f "${DIR}/Dockerfile" "${DIR}"
+podman build --pull=always -t quay.io/cryostat/cryostat3-db:latest -f "${DIR}/Dockerfile" "${DIR}"
 podman tag quay.io/cryostat/cryostat3-db:latest quay.io/cryostat/cryostat3-db:dev
