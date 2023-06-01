17a2e54b976fa46f549cd02c5ae4b0e29b708354
Andrew Azores <aazores@redhat.com>
2023-06-01 12:16:32 -0400

    port over simple itest from old codebase

---
+++ src/test/java/itest/bases/StandardSelfTest.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright The Cryostat Authors
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or data
+ * (collectively the "Software"), free of charge and under any and all copyright
+ * rights in the Software, and any and all patent rights owned or freely
+ * licensable by each licensor hereunder covering either (i) the unmodified
+ * Software as contributed to or provided by such licensor, or (ii) the Larger
+ * Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software (each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ * The above copyright notice and either this complete permission notice or at
+ * a minimum a reference to the UPL must be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package itest.bases;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import io.cryostat.util.HttpStatusCodeIdentifier;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.MultiMap;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.FileSystem;
+import io.vertx.core.http.WebSocket;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.handler.HttpException;
+import itest.util.Utils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+public abstract class StandardSelfTest {
+
+    public static final String SELF_REFERENCE_TARGET_ID =
+            URLEncodedUtils.formatSegments("service:jmx:rmi:///jndi/rmi://localhost:9091/jmxrmi");
+    public static final Pair<String, String> VERTX_FIB_CREDENTIALS =
+            Pair.of("admin", "adminpass123");
+
+    public static final int REQUEST_TIMEOUT_SECONDS = 30;
+    public static final WebClient webClient = Utils.getWebClient();
+
+    public static CompletableFuture<JsonObject> expectNotification(
+            String category, long timeout, TimeUnit unit)
+            throws TimeoutException, ExecutionException, InterruptedException {
+        CompletableFuture<JsonObject> future = new CompletableFuture<>();
+
+        Utils.HTTP_CLIENT.webSocket(
+                getNotificationsUrl().get(REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS),
+                ar -> {
+                    if (ar.failed()) {
+                        future.completeExceptionally(ar.cause());
+                        return;
+                    }
+                    WebSocket ws = ar.result();
+
+                    ws.handler(
+                                    m -> {
+                                        JsonObject resp = m.toJsonObject();
+                                        JsonObject meta = resp.getJsonObject("meta");
+                                        String c = meta.getString("category");
+                                        if (Objects.equals(c, category)) {
+                                            ws.end(unused -> future.complete(resp));
+                                            ws.close();
+                                        }
+                                    })
+                            // just to initialize the connection - Cryostat expects
+                            // clients to send a message after the connection opens
+                            // to authenticate themselves, but in itests we don't
+                            // use auth
+                            .writeTextMessage("");
+                });
+
+        return future.orTimeout(timeout, unit);
+    }
+
+    public static boolean assertRequestStatus(
+            AsyncResult<HttpResponse<Buffer>> result, CompletableFuture<?> future) {
+        if (result.failed()) {
+            result.cause().printStackTrace();
+            future.completeExceptionally(result.cause());
+
+            return false;
+        }
+        HttpResponse<Buffer> response = result.result();
+        if (!HttpStatusCodeIdentifier.isSuccessCode(response.statusCode())) {
+            System.err.println("HTTP " + response.statusCode() + ": " + response.statusMessage());
+            future.completeExceptionally(
+                    new HttpException(response.statusCode(), response.statusMessage()));
+            return false;
+        }
+        return true;
+    }
+
+    private static Future<String> getNotificationsUrl() {
+        CompletableFuture<String> future = new CompletableFuture<>();
+        webClient
+                .get("/api/v1/notifications_url")
+                .send(
+                        ar -> {
+                            if (ar.succeeded()) {
+                                future.complete(
+                                        ar.result()
+                                                .bodyAsJsonObject()
+                                                .getString("notificationsUrl"));
+                            } else {
+                                future.completeExceptionally(ar.cause());
+                            }
+                        });
+        return future;
+    }
+
+    public static CompletableFuture<Path> downloadFile(String url, String name, String suffix) {
+        return fireDownloadRequest(
+                webClient.get(url), name, suffix, MultiMap.caseInsensitiveMultiMap());
+    }
+
+    public static CompletableFuture<Path> downloadFileAbs(String url, String name, String suffix) {
+        return fireDownloadRequest(
+                webClient.getAbs(url), name, suffix, MultiMap.caseInsensitiveMultiMap());
+    }
+
+    public static CompletableFuture<Path> downloadFileAbs(
+            String url, String name, String suffix, MultiMap headers) {
+        return fireDownloadRequest(webClient.getAbs(url), name, suffix, headers);
+    }
+
+    private static CompletableFuture<Path> fireDownloadRequest(
+            HttpRequest<Buffer> request, String filename, String fileSuffix, MultiMap headers) {
+        CompletableFuture<Path> future = new CompletableFuture<>();
+        request.putHeaders(headers)
+                .send(
+                        ar -> {
+                            if (ar.failed()) {
+                                future.completeExceptionally(ar.cause());
+                                return;
+                            }
+                            HttpResponse<Buffer> resp = ar.result();
+                            if (resp.statusCode() != 200) {
+                                future.completeExceptionally(
+                                        new Exception(String.format("HTTP %d", resp.statusCode())));
+                                return;
+                            }
+                            FileSystem fs = Utils.getFileSystem();
+                            String file = fs.createTempFileBlocking(filename, fileSuffix);
+                            fs.writeFileBlocking(file, ar.result().body());
+                            future.complete(Paths.get(file));
+                        });
+        return future;
+    }
+}
