9d94b3edbe47c2f60cb36dabeaca2442f0df1f28
Ming Wang <miwan@redhat.com>
2023-06-28 14:43:47 -0400

    tmp
    change dir
    
    update README
    
    k8s
    
    k8s
    
    tmp
    
    local build
    
    tmp
    
    tmp
    
    tmp
    
    tmp
    
    load images separately
    
    tmp
    
    always
    
    update README for k8s
    
    tmp
    
    remove env vars
    
    add servers.json
    
    add servers.json
    
    connect to db-viewer

--- smoketest/k8s/smoketest.sh
+++ smoketest/k8s/smoketest.sh
@@ -17,19 +17,19 @@ while [ "$#" -ne 0 ]; do
         kind)
             cleanKind
             kind create cluster
-            kind load docker-image \
-                "quay.io/${IMAGE_REPOSITORY}/cryostat3:dev" \
-                "quay.io/${IMAGE_REPOSITORY}/cryostat3-db:dev"
+            kind load docker-image "quay.io/${IMAGE_REPOSITORY}/cryostat3:dev"
+            kind load docker-image "quay.io/${IMAGE_REPOSITORY}/cryostat3-db:dev" 
             ;;
         unkind)
             cleanKind
             ;;
         generate)
-            sh "${DIR}/../../db/build.sh"
-            kompose convert -o "${DIR}" -f "${DIR}/../../container-compose.yml"
+            sh "./../../db/build.sh"
+            kompose convert -o "./" -f "./../compose/db_k8s.yml" -f "./../compose/s3-minio.yml" -f "./../compose/cryostat_k8s.yml" -f "./../compose/sample-apps.yml" --build local
+            sh generate.sh
             ;;
         apply)
-            kubectl apply -f "${DIR}/*.yaml"
+            kubectl apply -f "./*.yaml"
             kubectl patch -p "{\"spec\":{\"template\":{\"spec\":{\"\$setElementOrder/containers\":[{\"name\":\"db\"}],\"containers\":[{\"image\":\"quay.io/$IMAGE_REPOSITORY/cryostat3-db:dev\",\"name\":\"db\"}]}}}}" deployment/db
             kubectl wait \
                 --for condition=available \
@@ -40,18 +40,17 @@ while [ "$#" -ne 0 ]; do
                 --for condition=available \
                 --timeout=5m \
                 deployment cryostat
-            echo "!!!! You must now 'kubectl edit deployment cryostat' and set the values for QUARKUS_MINIO_ACCESS_KEY, QUARKUS_MINIO_ACCESS_SECRET"
             ;;
         clean)
-            kubectl delete -f "${DIR}/*.yaml"
+            kubectl delete -f "./*.yaml"
             ;;
         forward)
             sh -c '(sleep 1 ; xdg-open http://localhost:9001 ; xdg-open http://localhost:8181 ; xdg-open http://localhost:8989)&'
             if ! kubectl multiforward smoketest; then
                 echo "Run the following to expose the applications:"
                 echo "kubectl port-forward svc/cryostat 8181"
-                echo "kubectl port-forward svc/minio 9001"
-                echo "kubectl port-forward svc/pgadmin 8989"
+                echo "kubectl port-forward svc/s3 9001"
+                echo "kubectl port-forward svc/db-viewer 8989"
             fi
             ;;
         *)
