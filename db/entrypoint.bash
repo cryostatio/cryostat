b6ce58433bb97b9edebedbe73a4f73d313c4936d
Andrew Azores <aazores@redhat.com>
2023-10-25 15:08:53 -0400

    feat(db): set custom entrypoint script that enforces $PG_ENCRYPT_KEY

---
+++ db/entrypoint.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ "${DEBUG}" = "true" ]; then
+    set -xEeo pipefail
+fi
+
+if [ -z "${PG_ENCRYPT_KEY}" ]; then
+    echo "\$PG_ENCRYPT_KEY must be set and non-empty."
+    exit 1
+fi
+
+exec /usr/local/bin/docker-entrypoint.sh postgres -c encrypt.key="${PG_ENCRYPT_KEY}" "$@"
