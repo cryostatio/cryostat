5c9897d577855f38a78aad39f8614999767830f5
Andrew Azores <aazores@redhat.com>
2023-10-26 14:10:55 -0400

    whitespace

--- db/entrypoint.bash
+++ db/entrypoint.bash
@@ -8,4 +8,7 @@ if [ "$1" = "postgres" ]; then
     shift
 fi
 
-exec /usr/local/bin/postgres-entrypoint.sh postgres -c encrypt.key="${PG_ENCRYPT_KEY?:\$PG_ENCRYPT_KEY must be set and non-empty}" "$@"
+exec /usr/local/bin/postgres-entrypoint.sh \
+    postgres \
+    -c encrypt.key="${PG_ENCRYPT_KEY?:\$PG_ENCRYPT_KEY must be set and non-empty}" \
+    "$@"
