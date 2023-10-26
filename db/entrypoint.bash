dc183fd91a0bf2c3b17c1f0d3aaa840747f4de82
Andrew Azores <aazores@redhat.com>
2023-10-26 14:10:08 -0400

    simplify

--- db/entrypoint.bash
+++ db/entrypoint.bash
@@ -4,13 +4,8 @@ if [ "${DEBUG}" = "true" ]; then
     set -xEeo pipefail
 fi
 
-if [ -z "${PG_ENCRYPT_KEY}" ]; then
-    echo "\$PG_ENCRYPT_KEY must be set and non-empty."
-    exit 1
-fi
-
 if [ "$1" = "postgres" ]; then
     shift
 fi
 
-exec /usr/local/bin/postgres-entrypoint.sh postgres -c encrypt.key="${PG_ENCRYPT_KEY}" "$@"
+exec /usr/local/bin/postgres-entrypoint.sh postgres -c encrypt.key="${PG_ENCRYPT_KEY?:\$PG_ENCRYPT_KEY must be set and non-empty}" "$@"
