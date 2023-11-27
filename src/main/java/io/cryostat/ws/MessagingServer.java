07c5a5773df394bd6df03a940ae48b8031ffb00d
Andrew Azores <aazores@redhat.com>
2023-11-27 17:11:20 -0500

    fix(messaging): handle writing to websockets on dedicated worker thread

--- src/main/java/io/cryostat/ws/MessagingServer.java
+++ src/main/java/io/cryostat/ws/MessagingServer.java
@@ -16,15 +16,23 @@
 package io.cryostat.ws;
 
 import java.io.IOException;
+import java.nio.channels.ClosedChannelException;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
 import io.quarkus.vertx.ConsumeEvent;
 import io.smallrye.common.annotation.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnError;
@@ -32,6 +40,8 @@ import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 @ApplicationScoped
@@ -39,8 +49,14 @@ import org.jboss.logging.Logger;
 public class MessagingServer {
 
     @Inject Logger logger;
-    private final Set<Session> sessions = ConcurrentHashMap.newKeySet();
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private final BlockingQueue<Notification> msgQ;
+    private final Set<Session> sessions = new CopyOnWriteArraySet<>();
     private final ObjectMapper mapper = new ObjectMapper();
+
+    MessagingServer(@ConfigProperty(name = "cryostat.messaging.queue.size") int capacity) {
+        this.msgQ = new ArrayBlockingQueue<>(capacity);
+    }
 
     // TODO implement authentication check
     @OnOpen
@@ -62,42 +78,70 @@ public class MessagingServer {
             logger.errorv("Closing session {0}", session.getId());
             session.close();
         } catch (IOException ioe) {
-            ioe.printStackTrace(System.err);
             logger.error("Unable to close session", ioe);
         }
     }
 
+    void start(@Observes StartupEvent evt) {
+        logger.infov("Starting {0} executor", getClass().getName());
+        executor.execute(
+                () -> {
+                    while (!executor.isShutdown()) {
+                        try {
+                            var notification = msgQ.take();
+                            var map =
+                                    Map.of(
+                                            "meta",
+                                            Map.of("category", notification.category()),
+                                            "message",
+                                            notification.message());
+                            logger.infov("Broadcasting: {0}", map);
+                            sessions.forEach(
+                                    s -> {
+                                        try {
+                                            s.getBasicRemote()
+                                                    .sendText(mapper.writeValueAsString(map));
+                                        } catch (JsonProcessingException e) {
+                                            logger.error("Unable to serialize message to JSON", e);
+                                        } catch (IOException e) {
+                                            // ignored simple ClosedChannelExceptions since this
+                                            // just means the connection has already been closed,
+                                            // either due to an error or the client closing it. This
+                                            // does not actually indicate a problem
+                                            if (!ExceptionUtils.getThrowableList(e).stream()
+                                                    .anyMatch(
+                                                            t ->
+                                                                    t
+                                                                            instanceof
+                                                                            ClosedChannelException)) {
+                                                logger.errorv(
+                                                        "Unable to send message to {0}", s.getId());
+                                                logger.error(e);
+                                            }
+                                        }
+                                    });
+                        } catch (InterruptedException ie) {
+                            logger.warn(ie);
+                            break;
+                        }
+                    }
+                });
+    }
+
+    void shutdown(@Observes ShutdownEvent evt) {
+        logger.infov("Shutting down {0} executor", getClass().getName());
+        executor.shutdown();
+        msgQ.clear();
+    }
+
     @OnMessage
     public void onMessage(Session session, String message) {
-        logger.infov("[{0}] message: {1}", session.getId(), message);
+        logger.infov("{0} message: \"{1}\"", session.getId(), message);
     }
 
     @ConsumeEvent
     @Blocking
     void broadcast(Notification notification) {
-        var map =
-                Map.of(
-                        "meta",
-                        Map.of("category", notification.category()),
-                        "message",
-                        notification.message());
-        logger.infov("Broadcasting: {0}", map);
-        sessions.forEach(
-                s -> {
-                    try {
-                        s.getAsyncRemote()
-                                .sendObject(
-                                        mapper.writeValueAsString(map),
-                                        result -> {
-                                            if (result.getException() != null) {
-                                                logger.warn(
-                                                        "Unable to send message: "
-                                                                + result.getException());
-                                            }
-                                        });
-                    } catch (JsonProcessingException e) {
-                        logger.error("Unable to send message", e);
-                    }
-                });
+        msgQ.add(notification);
     }
 }
