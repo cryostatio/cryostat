7b0cf11df6ef677b23bb87d39a5145cc32988464
Andrew Azores <aazores@redhat.com>
2024-02-15 11:49:19 -0500

    tmp

--- src/main/java/io/cryostat/events/S3TemplateService.java
+++ src/main/java/io/cryostat/events/S3TemplateService.java
@@ -15,13 +15,18 @@
  */
 package io.cryostat.events;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
 import org.openjdk.jmc.common.unit.IConstrainedMap;
+import org.openjdk.jmc.common.unit.SimpleConstrainedMap;
+import org.openjdk.jmc.common.unit.UnitLookup;
 import org.openjdk.jmc.flightrecorder.configuration.events.EventOptionID;
 import org.openjdk.jmc.flightrecorder.controlpanel.ui.configuration.model.xml.JFCGrammar;
 import org.openjdk.jmc.flightrecorder.controlpanel.ui.configuration.model.xml.XMLAttributeInstance;
@@ -47,10 +52,13 @@ import jakarta.inject.Inject;
 import org.apache.hc.core5.http.ContentType;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -93,33 +101,117 @@ class S3TemplateService implements TemplateService {
 
     @Override
     public Optional<IConstrainedMap<EventOptionID>> getEvents(
-            String templateName, TemplateType templateType) throws FlightRecorderException {
-        return Optional.empty();
+            String templateName, TemplateType unused) throws FlightRecorderException {
+        return getObject(templateName)
+                .map(this::getContents)
+                .map(
+                        stream -> {
+                            try {
+                                return new EventConfiguration(parseXml(stream))
+                                        .getEventOptions(
+                                                new SimpleConstrainedMap<>(
+                                                        UnitLookup.PLAIN_TEXT.getPersister()));
+                            } catch (IOException | ParseException e) {
+                                logger.error(e);
+                                return null;
+                            }
+                        });
     }
 
     @Override
     public List<Template> getTemplates() throws FlightRecorderException {
-        var builder = ListObjectsV2Request.builder().bucket(eventTemplatesBucket);
-        var objects = storage.listObjectsV2(builder.build()).contents();
-        var templates = convertObjects(objects);
-        return templates;
-    }
-
-    private List<Template> convertObjects(List<S3Object> objects) {
-        return List.of();
+        return convertObjects(getObjects());
     }
 
     @Override
-    public Optional<Document> getXml(String templateName, TemplateType templateType)
+    public Optional<Document> getXml(String templateName, TemplateType unused)
             throws FlightRecorderException {
-        return Optional.empty();
+        return getObject(templateName)
+                .map(this::getContents)
+                .map(
+                        stream -> {
+                            try (stream) {
+                                Document doc =
+                                        Jsoup.parse(
+                                                stream,
+                                                StandardCharsets.UTF_8.name(),
+                                                "",
+                                                Parser.xmlParser());
+                                return doc;
+                            } catch (IOException e) {
+                                logger.error(e);
+                                return null;
+                            }
+                        });
     }
 
     @Blocking
-    public Template addTemplate(String templateText)
-            throws InvalidXmlException, InvalidEventTemplateException, IOException {
-        try {
-            XMLModel model = EventConfiguration.createModel(templateText);
+    private Optional<S3Object> getObject(String name) {
+        // FIXME do this by querying for a single S3Object rather than list and filter
+        return getObjects().stream().filter(o -> o.key().equals(name)).findFirst();
+    }
+
+    @Blocking
+    private List<S3Object> getObjects() {
+        var builder = ListObjectsV2Request.builder().bucket(eventTemplatesBucket);
+        return storage.listObjectsV2(builder.build()).contents();
+    }
+
+    private List<Template> convertObjects(List<S3Object> objects) {
+        return objects.stream()
+                .map(
+                        t -> {
+                            try {
+                                return convertObject(t);
+                            } catch (IOException
+                                    | ParseException
+                                    | InvalidEventTemplateException e) {
+                                logger.error(e);
+                                return null;
+                            }
+                        })
+                .toList();
+    }
+
+    private Template convertObject(S3Object object)
+            throws IOException, ParseException, InvalidEventTemplateException {
+        var xml = parseXml(getContents(object));
+
+        // XMLAttributeInstance descAttr = null, providerAttr = null;
+        // var configuration = xml.getRoot();
+        // for (var attr : configuration.getAttributeInstances()) {
+        //     if (attr.getAttribute().getName().equals("description")) {
+        //         descAttr = attr;
+        //         break;
+        //     } else if (attr.getAttribute().getName().equals("provider")) {
+        //         providerAttr = attr;
+        //         break;
+        //     }
+        // }
+        // if (descAttr == null) {
+        //     throw new InvalidEventTemplateException("\"description\" attribute not found");
+        // }
+        // if (providerAttr == null) {
+        //     throw new InvalidEventTemplateException("\"provider\" attribute not found");
+        // }
+
+        return new Template(
+                object.key(),
+                "",
+                "unknown",
+                // descAttr.getExplicitValue(),
+                // providerAttr.getExplicitValue(),
+                TemplateType.CUSTOM);
+    }
+
+    private InputStream getContents(S3Object object) {
+        var req = GetObjectRequest.builder().bucket(eventTemplatesBucket).key(object.key()).build();
+        return storage.getObject(req);
+    }
+
+    private XMLModel parseXml(InputStream inputStream) throws IOException, ParseException {
+        try (inputStream) {
+            var model = EventConfiguration.createModel(inputStream);
             model.checkErrors();
 
             for (XMLValidationResult result : model.getResults()) {
@@ -128,6 +220,18 @@ class S3TemplateService implements TemplateService {
                             new InvalidEventTemplateException(result.getText()));
                 }
             }
+            return model;
+        }
+    }
+
+    @Blocking
+    public Template addTemplate(String templateText)
+            throws InvalidXmlException, InvalidEventTemplateException, IOException {
+        try {
+            XMLModel model =
+                    parseXml(
+                            new ByteArrayInputStream(
+                                    templateText.getBytes(StandardCharsets.UTF_8)));
 
             XMLTagInstance configuration = model.getRoot();
             XMLAttributeInstance labelAttr = null;
@@ -150,6 +254,8 @@ class S3TemplateService implements TemplateService {
             XMLTagInstance root = model.getRoot();
             root.setValue(JFCGrammar.ATTRIBUTE_LABEL_MANDATORY, templateName);
 
+            // TODO put the template description, provider, and other attributes in metadata so we
+            // don't need to download and parse the whole XML just to display the templates list
             String key = templateName;
             storage.putObject(
                     PutObjectRequest.builder()
