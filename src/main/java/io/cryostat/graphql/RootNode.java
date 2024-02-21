b3ef3093535f72aa86ff14b0f82c8587a9365c3f
Andrew Azores <aazores@redhat.com>
2024-02-21 15:11:07 -0500

    add basic rootNode query

---
+++ src/main/java/io/cryostat/graphql/RootNode.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright The Cryostat Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.cryostat.graphql;
+
+import io.cryostat.discovery.DiscoveryNode;
+
+import org.eclipse.microprofile.graphql.Description;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
+
+@GraphQLApi
+public class RootNode {
+
+    @Query("rootNode")
+    @Description("Get the root target discovery node")
+    public DiscoveryNode getRootNode() {
+        return DiscoveryNode.getUniverse();
+    }
+}
