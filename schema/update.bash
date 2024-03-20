4680fca295f9c02b281fe84b5b4674c74b1a5fcd
Andrew Azores <aazores@redhat.com>
2024-03-20 11:23:40 -0400

    fix broken loop condition

--- schema/update.bash
+++ schema/update.bash
@@ -8,7 +8,7 @@ pid="$!"
 set +e
 sleep 30
 wget --spider http://localhost:8181/health
-while [  -ne 0 ]; do !! ; sleep 10 ; done
+while [ $? -ne 0 ]; do !! ; sleep 10 ; done
 wget http://localhost:8181/api -O - | yq -P 'sort_keys(..)' > "${DIR}/openapi.yaml"
 wget http://localhost:8181/api/v3/graphql/schema.graphql -O "${DIR}/schema.graphql"
 kill $pid
