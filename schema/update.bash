988b9a4533d616e223d15a9eff827d3880af9445
Andrew Azores <aazores@redhat.com>
2024-03-20 11:35:02 -0400

    better healthcheck loop

--- schema/update.bash
+++ schema/update.bash
@@ -7,8 +7,18 @@ DIR="$(dirname "$(readlink -f "$0")")"
 pid="$!"
 set +e
 sleep 30
-wget --spider http://localhost:8181/health
-while [ $? -ne 0 ]; do !! ; sleep 10 ; done
+counter=0
+while true; do
+    if [ "${counter}" -gt 10 ]; then
+        exit 1
+    fi
+    if wget --spider http://localhost:8181/health; then
+        break
+    else
+        counter=$((counter + 1))
+        sleep 10
+    fi
+done
 wget http://localhost:8181/api -O - | yq -P 'sort_keys(..)' > "${DIR}/openapi.yaml"
 wget http://localhost:8181/api/v3/graphql/schema.graphql -O "${DIR}/schema.graphql"
 kill $pid
