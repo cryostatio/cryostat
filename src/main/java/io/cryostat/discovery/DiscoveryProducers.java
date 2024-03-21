65ad1ca6d101134a34b1375b9698907d44506459
Andrew Azores <aazores@redhat.com>
2024-03-21 14:47:57 -0400

    configurable JWT algo and keysize

--- src/main/java/io/cryostat/discovery/DiscoveryProducers.java
+++ src/main/java/io/cryostat/discovery/DiscoveryProducers.java
@@ -15,71 +15,59 @@
  */
 package io.cryostat.discovery;
 
+import java.security.NoSuchAlgorithmException;
+
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWEDecrypter;
 import com.nimbusds.jose.JWEEncrypter;
 import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.KeyLengthException;
 import com.nimbusds.jose.crypto.DirectDecrypter;
 import com.nimbusds.jose.crypto.DirectEncrypter;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.MACVerifier;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 public class DiscoveryProducers {
 
     @Produces
     @ApplicationScoped
-    static SecretKey provideSecretKey() {
-        try {
-            KeyGenerator generator = KeyGenerator.getInstance("AES");
-            generator.init(256);
-            return generator.generateKey();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    static SecretKey provideSecretKey(
+            @ConfigProperty(name = "cryostat.discovery.plugins.jwt.algorithm") String alg,
+            @ConfigProperty(name = "cryostat.discovery.plugins.jwt.keysize") int keysize)
+            throws NoSuchAlgorithmException {
+        KeyGenerator generator = KeyGenerator.getInstance(alg);
+        generator.init(keysize);
+        return generator.generateKey();
     }
 
     @Produces
     @ApplicationScoped
-    static JWSSigner provideJwsSigner(SecretKey key) {
-        try {
-            return new MACSigner(key);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    static JWSSigner provideJwsSigner(SecretKey key) throws KeyLengthException {
+        return new MACSigner(key);
     }
 
     @Produces
     @ApplicationScoped
-    static JWSVerifier provideJwsVerifier(SecretKey key) {
-        try {
-            return new MACVerifier(key);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    static JWSVerifier provideJwsVerifier(SecretKey key) throws JOSEException {
+        return new MACVerifier(key);
     }
 
     @Produces
     @ApplicationScoped
-    static JWEEncrypter provideJweEncrypter(SecretKey key) {
-        try {
-            return new DirectEncrypter(key);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    static JWEEncrypter provideJweEncrypter(SecretKey key) throws KeyLengthException {
+        return new DirectEncrypter(key);
     }
 
     @Produces
     @ApplicationScoped
-    static JWEDecrypter provideJweDecrypter(SecretKey key) {
-        try {
-            return new DirectDecrypter(key);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    static JWEDecrypter provideJweDecrypter(SecretKey key) throws KeyLengthException {
+        return new DirectDecrypter(key);
     }
 }
