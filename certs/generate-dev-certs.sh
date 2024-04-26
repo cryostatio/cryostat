3c8106f07fd4a702308e78722c9d4f8913439582
Ming Wang <miwan@redhat.com>
2024-04-26 14:54:17 -0400

    avoid hardcoded java openjdk path

--- certs/generate-dev-certs.sh
+++ certs/generate-dev-certs.sh
@@ -10,9 +10,13 @@ SSL_TRUSTSTORE=cryostat-truststore.p12
 
 SSL_KEYSTORE_PASS_FILE=keystore.pass
 
+if [ -z "$JAVA_HOME" ]; then
+    JAVA_HOME=$(readlink -f /usr/bin/java | grep -oP '.*?openjdk')
+fi
+
 cleanup() {
     cd "$CERTS_DIR"
-    rm $SSL_TRUSTSTORE $SSL_KEYSTORE $SSL_KEYSTORE_PASS_FILE
+    rm -f $SSL_TRUSTSTORE $SSL_KEYSTORE $SSL_KEYSTORE_PASS_FILE
     cd -
 }
 
@@ -42,16 +46,11 @@ SSL_KEYSTORE_PASS=$(genpass)
 cd "$CERTS_DIR"
 trap "cd -" EXIT
 
-echo "$SSL_KEYSTORE_PASS" > $SSL_KEYSTORE_PASS_FILE
+if [ -f $SSL_KEYSTORE_PASS_FILE ]; then
+    cleanup
+fi
 
-keytool \
-    -importkeystore \
-    -noprompt \
-    -storetype PKCS12 \
-    -srckeystore /usr/lib/jvm/java-11-openjdk/lib/security/cacerts \
-    -srcstorepass changeit \
-    -destkeystore "$SSL_TRUSTSTORE" \
-    -deststorepass "$SSL_TRUSTSTORE_PASS"
+echo "$SSL_KEYSTORE_PASS" > $SSL_KEYSTORE_PASS_FILE
 
 keytool \
     -genkeypair -v \
@@ -70,13 +69,25 @@ keytool \
     -storepass "$SSL_KEYSTORE_PASS" \
     -file server.cer
 
-keytool \
-    -importcert -v \
-    -noprompt \
-    -trustcacerts \
-    -keystore "$SSL_TRUSTSTORE" \
-    -alias selftrust \
-    -file server.cer \
-    -storepass "$SSL_TRUSTSTORE_PASS"
+if [ -d "$CERTS_DIR/../truststore" ]; then
 
-mv server.cer "$CERTS_DIR/../truststore/dev-self-signed.cer"
+    keytool \
+    -importkeystore \
+    -noprompt \
+    -storetype PKCS12 \
+    -srckeystore ${JAVA_HOME}/lib/security/cacerts \
+    -srcstorepass changeit \
+    -destkeystore "$SSL_TRUSTSTORE" \
+    -deststorepass "$SSL_TRUSTSTORE_PASS"
+
+    keytool \
+        -importcert -v \
+        -noprompt \
+        -trustcacerts \
+        -keystore "$SSL_TRUSTSTORE" \
+        -alias selftrust \
+        -file server.cer \
+        -storepass "$SSL_TRUSTSTORE_PASS"
+
+   mv server.cer "$CERTS_DIR/../truststore/dev-self-signed.cer"
+fi
