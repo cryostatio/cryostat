ca304a4e3e6ce3fb34ab292cae69c743717e3dd2
Ming Wang <miwan@redhat.com>
2024-08-04 13:01:14 -0400

    SAN for auth cert

--- compose/auth_certs/generate.sh
+++ compose/auth_certs/generate.sh
@@ -4,4 +4,4 @@ set -xe
 
 CERTS_DIR="$(dirname "$(readlink -f "$0")")"
 
-openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out "${CERTS_DIR}/certificate.pem" -keyout "${CERTS_DIR}/private.key"
+openssl req -new -addext "subjectAltName = DNS:auth" -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out "${CERTS_DIR}/certificate.pem" -keyout "${CERTS_DIR}/private.key"
