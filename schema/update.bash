c580afd1ecca5964bd4de85b6995ba337239e482
Andrew Azores <aazores@redhat.com>
2024-08-28 11:24:33 -0400

    schema update script works with httpie

--- schema/update.bash
+++ schema/update.bash
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+set -x
+
 DIR="$(dirname "$(readlink -f "$0")")"
 
-"${DIR}"/../mvnw -f "${DIR}/../pom.xml" -B -U -Dspotless.check.skip -Dquarkus.quinoa=false clean compile test-compile
+if ! command -v httpz && ! command -v wget; then
+    echo "No HTTPie or wget?"
+    exit 1
+fi
+
+"${DIR}"/../mvnw -f "${DIR}/../pom.xml" -B -U -Dspotless.check.skip clean compile test-compile
 "${DIR}"/../mvnw -f "${DIR}/../pom.xml" -B -U -Dmaven.test.skip -Dquarkus.quinoa=false -Dspotless.check.skip -Dquarkus.smallrye-openapi.info-title="Cryostat API" clean quarkus:dev &
 pid="$!"
 function cleanup() {
@@ -16,12 +23,26 @@ while true; do
     if [ "${counter}" -gt 10 ]; then
         exit 1
     fi
-    if wget --spider http://localhost:8181/health; then
-        break
-    else
-        counter=$((counter + 1))
-        sleep "${2:-10}"
+    if command -v http; then
+        if http :8181/health/liveness; then
+            break
+        else
+            counter=$((counter + 1))
+            sleep "${2:-10}"
+        fi
+    elif command -v wget; then
+        if wget --tries=1 --spider http://localhost:8181/health/liveness; then
+            break
+        else
+            counter=$((counter + 1))
+            sleep "${2:-10}"
+        fi
     fi
 done
-wget http://localhost:8181/api -O - | yq -P 'sort_keys(..)' > "${DIR}/openapi.yaml"
-wget http://localhost:8181/api/v4/graphql/schema.graphql -O "${DIR}/schema.graphql"
+if command -v http; then
+    http --pretty=format --body :8181/api | yq -P 'sort_keys(..)' > "${DIR}/openapi.yaml"
+    http --pretty=format --body :8181/api/v4/graphql/schema.graphql > "${DIR}/schema.graphql"
+elif command -v wget; then
+    wget http://localhost:8181/api -O - | yq -P 'sort_keys(..)' > "${DIR}/openapi.yaml"
+    wget http://localhost:8181/api/v4/graphql/schema.graphql -O "${DIR}/schema.graphql"
+fi
