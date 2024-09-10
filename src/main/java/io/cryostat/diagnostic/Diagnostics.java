08ae0f625043fbd9ddc9a86f36649636c46d4645
Andrew Azores <aazores@redhat.com>
2024-09-10 16:16:26 -0400

    feat(diagnostic): implement diagnostics class with gc invocation operation

---
+++ src/main/java/io/cryostat/diagnostic/Diagnostics.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright The Cryostat Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.cryostat.diagnostic;
+
+import io.cryostat.targets.Target;
+import io.cryostat.targets.TargetConnectionManager;
+
+import io.smallrye.common.annotation.Blocking;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import org.jboss.resteasy.reactive.RestPath;
+
+@Path("/api/beta/diagnostics/targets/{targetId}")
+public class Diagnostics {
+
+    @Inject TargetConnectionManager targetConnectionManager;
+
+    @Path("/gc")
+    @RolesAllowed("write")
+    @Blocking
+    @POST
+    public void gc(@RestPath long targetId) {
+        targetConnectionManager.executeConnectedTask(
+                Target.getTargetById(targetId),
+                conn -> conn.invokeMBeanOperation("java.lang:type=Memory", "gc", null, null, Void.class));
+    }
+}
