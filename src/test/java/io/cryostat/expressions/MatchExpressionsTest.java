329fec6dcda59b3d6f51c6dbb1dd944fecc04031
Andrew Azores <aazores@redhat.com>
2024-12-11 11:55:11 -0500

    rename function for clarity

--- src/test/java/io/cryostat/expressions/MatchExpressionsTest.java
+++ src/test/java/io/cryostat/expressions/MatchExpressionsTest.java
@@ -80,9 +80,9 @@ public class MatchExpressionsTest extends AbstractTransactionalTestBase {
                 "this is garbage | 400 | false",
                 "target.alias == 'selftest' | 200 | true",
                 "target.alias == '' | 200 | false",
-                "eventTypeIds(target).exists(x, x.contains('jdk')) | 200 | true",
-                "eventTypeIds(target).exists(t, t.contains('nonsense')) | 200 | false",
-                "eventTypeIds(target).exists(x, t.contains('wrong binding')) | 400 | false",
+                "jfrEventTypeIds(target).exists(x, x.contains('jdk')) | 200 | true",
+                "jfrEventTypeIds(target).exists(t, t.contains('nonsense')) | 200 | false",
+                "jfrEventTypeIds(target).exists(x, t.contains('wrong binding')) | 400 | false",
             })
     public void testExpressionTest(String expr, int status, boolean expectTargets) {
         int id = defineSelfCustomTarget();
