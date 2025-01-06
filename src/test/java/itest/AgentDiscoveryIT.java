38ae0ebd0a296303eb29efeab88016c66da5be1d
Andrew Azores <aazores@redhat.com>
2025-01-06 13:21:14 -0500

    enable test if CI_ARCH is unset or matches supported arch

--- src/test/java/itest/AgentDiscoveryIT.java
+++ src/test/java/itest/AgentDiscoveryIT.java
@@ -34,9 +34,12 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 @QuarkusIntegrationTest
 @QuarkusTestResource(value = AgentApplicationResource.class, restrictToAnnotatedClass = true)
+@EnabledIfEnvironmentVariable(named = "CI_ARCH", matches = "^$")
+@EnabledIfEnvironmentVariable(named = "CI_ARCH", matches = "^amd64|AMD64$")
 public class AgentDiscoveryIT extends HttpClientTest {
 
     static final Logger logger = Logger.getLogger(AgentDiscoveryIT.class);
