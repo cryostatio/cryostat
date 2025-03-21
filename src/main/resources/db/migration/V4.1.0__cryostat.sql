823aa158999ab20294a4140ccc9e4dbb1ce7076f
Andrew Azores <aazores@redhat.com>
2025-03-21 16:31:56 -0400

    reuse realm name

--- src/main/resources/db/migration/V4.1.0__cryostat.sql
+++ src/main/resources/db/migration/V4.1.0__cryostat.sql
@@ -1,16 +1,1 @@
 alter table Rule add column metadata jsonb default '{"labels":{}}';
-
-/* Select the Universe node, then insert Realm nodes for each builtin discovery plugin with the universe as their parent */
-with universe as (
-    select id from DiscoveryNode where (nodeType = 'Universe')
-)
-insert into DiscoveryNode(
-    id,
-    labels,
-    name,
-    nodeType,
-    parentNode
-) values
-((select nextval('DiscoveryNode_SEQ')), '{}'::jsonb, 'KubernetesEndpointSlices', 'Realm', (select id from universe));
-
-delete from DiscoveryNode where name='KubernetesApi';
