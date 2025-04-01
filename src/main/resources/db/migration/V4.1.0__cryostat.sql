d5a0dbd46ae13bb326884d0c48b15e9fe7ef1d41
Andrew Azores <aazores@redhat.com>
2025-04-01 10:07:32 -0400

    fixup! chore(sql): set up indices for perf

--- src/main/resources/db/migration/V4.1.0__cryostat.sql
+++ src/main/resources/db/migration/V4.1.0__cryostat.sql
@@ -2,7 +2,7 @@ alter table Rule add column metadata jsonb default '{"labels":{}}';
 
 alter sequence DiscoveryNode_SEQ increment by 50;
 
-create unique index on Rule (name);
+create index on Rule (name);
 
-create unique index on Target (jvmId);
-create unique index on Target (connectUrl);
+create index on Target (jvmId);
+create index on Target (connectUrl);
