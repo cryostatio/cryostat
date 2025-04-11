0ed4f7a369bd0f6464f4937acf9ae9f79ab67f68
Andrew Azores <aazores@redhat.com>
2025-04-11 12:51:47 -0400

    reorder

--- src/main/resources/db/migration/V4.1.0__cryostat.sql
+++ src/main/resources/db/migration/V4.1.0__cryostat.sql
@@ -1,11 +1,10 @@
-alter table Rule add column metadata jsonb default '{"labels":{}}';
-
 alter sequence DiscoveryNode_SEQ increment by 50;
-
-create index on Rule (name);
 
 create index on Target (jvmId);
 create index on Target (connectUrl);
 
 create index on DiscoveryNode (nodeType);
 create index on DiscoveryNode (nodeType, name);
+
+create index on Rule (name);
+alter table Rule add column metadata jsonb default '{"labels":{}}';
