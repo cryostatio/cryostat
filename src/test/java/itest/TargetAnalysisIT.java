d722a07843aaab37b1ae911852e1d2e59284aa31
Andrew Azores <aazores@redhat.com>
2025-06-25 16:57:36 -0400

    test fixup

--- src/test/java/itest/TargetAnalysisIT.java
+++ src/test/java/itest/TargetAnalysisIT.java
@@ -20,8 +20,10 @@ import static io.restassured.RestAssured.given;
 import java.io.IOException;
 import java.net.URI;
 import java.time.Duration;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
@@ -35,19 +37,47 @@ import io.smallrye.mutiny.TimeoutException;
 import io.vertx.core.MultiMap;
 import io.vertx.core.json.JsonObject;
 import itest.bases.HttpClientTest;
+import itest.resources.S3StorageResource;
 import jakarta.websocket.ClientEndpoint;
 import jakarta.websocket.ContainerProvider;
 import jakarta.websocket.DeploymentException;
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.Session;
 import org.hamcrest.Matchers;
+import org.jboss.logging.Logger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @QuarkusIntegrationTest
 @QuarkusTestResource(value = AgentApplicationResource.class, restrictToAnnotatedClass = true)
+@QuarkusTestResource(value = S3StorageResource.class, restrictToAnnotatedClass = true)
 public class TargetAnalysisIT extends HttpClientTest {
 
-    public static final String TEMPLATE_CONTINUOUS = "template=Continuous";
+    public static final String TEMPLATE_CONTINUOUS = "template=Continuous,type=TARGET";
+
+    static WebSocketClient WS_CLIENT;
+    static Session WS_SESSION;
+
+    @BeforeAll
+    static void setupWebSocketClient() throws IOException, DeploymentException {
+        WS_CLIENT = new WebSocketClient();
+        WS_SESSION =
+                ContainerProvider.getWebSocketContainer()
+                        .connectToServer(
+                                WS_CLIENT, URI.create("ws://localhost:8081/api/notifications"));
+    }
+
+    @BeforeEach
+    void clearWebSocketNotifications() {
+        WS_CLIENT.msgQ.clear();
+    }
+
+    @AfterAll
+    static void tearDownWebSocketClient() throws IOException {
+        WS_SESSION.close();
+    }
 
     @Test
     void testGetAgentTargetReport()
@@ -60,43 +90,55 @@ public class TargetAnalysisIT extends HttpClientTest {
         Target agent = getAgentTarget();
         long targetId = agent.id;
         String archivedRecordingName = null;
+        long recordingId = -1;
         try {
-            startRecording(targetId, "targetAnalysisReportRecording", TEMPLATE_CONTINUOUS);
+            recordingId =
+                    startRecording(targetId, "targetAnalysisReportRecording", TEMPLATE_CONTINUOUS);
 
-            String archiveJobId =
-                    given().log()
-                            .all()
-                            .when()
-                            .pathParams("targetId", targetId)
-                            .post("/api/v4.1/targets/{targetId}/reports")
-                            .then()
-                            .log()
-                            .all()
-                            .and()
-                            .assertThat()
-                            // 202 Indicates report generation is in progress and sends an
-                            // intermediate
-                            // response.
-                            .statusCode(202)
-                            .contentType(ContentType.TEXT)
-                            .body(Matchers.any(String.class))
-                            .assertThat()
-                            // Verify we get a location header from a 202.
-                            .header(
-                                    "Location",
-                                    String.format(
-                                            "http://localhost:8081/api/v4/targets/%d/reports",
-                                            targetId))
-                            .and()
-                            .extract()
-                            .body()
-                            .asString();
+            Thread.sleep(5_000);
+
+            String[] archiveJobId = new String[1];
+            Executors.newSingleThreadScheduledExecutor()
+                    .schedule(
+                            () -> {
+                                archiveJobId[0] =
+                                        given().log()
+                                                .all()
+                                                .when()
+                                                .pathParams("targetId", targetId)
+                                                .post("/api/v4.1/targets/{targetId}/reports")
+                                                .then()
+                                                .log()
+                                                .all()
+                                                .and()
+                                                .assertThat()
+                                                // 202 Indicates report generation is in progress
+                                                // and sends an
+                                                // intermediate
+                                                // response.
+                                                .statusCode(202)
+                                                .contentType(ContentType.TEXT)
+                                                .body(Matchers.any(String.class))
+                                                .assertThat()
+                                                // Verify we get a location header from a 202.
+                                                .header(
+                                                        "Location",
+                                                        String.format(
+                                                                "http://localhost:8081/api/v4.1/targets/%d/reports",
+                                                                targetId))
+                                                .and()
+                                                .extract()
+                                                .body()
+                                                .asString();
+                            },
+                            10,
+                            TimeUnit.SECONDS);
 
             JsonObject archiveMessage =
                     expectWebSocketNotification(
                             "ArchiveRecordingSuccess",
                             o ->
-                                    archiveJobId.equals(
+                                    archiveJobId[0].equals(
                                             o.getJsonObject("message").getString("jobId")));
             archivedRecordingName = archiveMessage.getJsonObject("message").getString("recording");
 
@@ -120,21 +162,47 @@ public class TargetAnalysisIT extends HttpClientTest {
                         .assertThat()
                         .statusCode(204);
             }
+
+            if (recordingId >= 0) {
+                given().log()
+                        .all()
+                        .when()
+                        .pathParams("targetId", targetId, "recordingId", recordingId)
+                        .delete("/api/v4/targets/{targetId}/recordings/{recordingId}")
+                        .then()
+                        .log()
+                        .all()
+                        .and()
+                        .assertThat()
+                        .statusCode(204);
+            }
         }
     }
 
-    private void startRecording(long targetId, String recordingName, String events)
+    private long startRecording(long targetId, String recordingName, String events)
             throws java.util.concurrent.TimeoutException, InterruptedException, ExecutionException {
         MultiMap form = MultiMap.caseInsensitiveMultiMap();
         form.add("recordingName", recordingName);
         form.add("duration", "5");
         form.add("events", events);
-        webClient
-                .extensions()
-                .post(
-                        String.format("/api/v4/targets/%d/recordings", targetId),
-                        form,
-                        REQUEST_TIMEOUT_SECONDS);
+        return given().log()
+                .all()
+                .pathParams("targetId", targetId)
+                .formParam("recordingName", recordingName)
+                .formParam("duration", "5")
+                .formParam("events", events)
+                .when()
+                .post("/api/v4/targets/{targetId}/recordings")
+                .then()
+                .log()
+                .all()
+                .and()
+                .assertThat()
+                .statusCode(201)
+                .and()
+                .extract()
+                .jsonPath()
+                .getLong("remoteId");
     }
 
     private Target getAgentTarget() throws InterruptedException {
@@ -152,6 +220,9 @@ public class TargetAnalysisIT extends HttpClientTest {
                             .log()
                             .all()
                             .and()
+                            .assertThat()
+                            .statusCode(200)
+                            .and()
                             .extract()
                             .body()
                             .as(Target[].class);
@@ -165,7 +236,18 @@ public class TargetAnalysisIT extends HttpClientTest {
         throw new IllegalStateException();
     }
 
-    private record Target(long id, String jvmId, String connectUrl, String alias) {}
+    private record Target(
+            long id,
+            String jvmId,
+            String connectUrl,
+            String alias,
+            Annotations annotations,
+            List<KeyValue> labels,
+            boolean agent) {}
+
+    private record Annotations(List<KeyValue> platform, List<KeyValue> cryostat) {}
+
+    private record KeyValue(String key, String value) {}
 
     protected JsonObject expectWebSocketNotification(String category)
             throws IOException, DeploymentException, InterruptedException, TimeoutException {
@@ -188,36 +270,33 @@ public class TargetAnalysisIT extends HttpClientTest {
             throws IOException, DeploymentException, InterruptedException, TimeoutException {
         long now = System.nanoTime();
         long deadline = now + timeout.toNanos();
-        var client = new WebSocketClient();
-        try (Session session =
-                ContainerProvider.getWebSocketContainer()
-                        .connectToServer(
-                                client, URI.create("ws://localhost:8181/api/notifications"))) {
-            do {
-                now = System.nanoTime();
-                String msg = client.wsMessages.poll(1, TimeUnit.SECONDS);
-                if (msg == null) {
-                    continue;
-                }
-                JsonObject obj = new JsonObject(msg);
-                String msgCategory = obj.getJsonObject("meta").getString("category");
-                if (category.equals(msgCategory) && predicate.test(obj)) {
-                    return obj;
-                }
-            } while (now < deadline);
-        } finally {
-            client.wsMessages.clear();
-        }
+        logger.infov(
+                "waiting up to {0} for a WebSocket notification with category={1}",
+                timeout, category);
+        do {
+            now = System.nanoTime();
+            String msg = WS_CLIENT.msgQ.poll(1, TimeUnit.SECONDS);
+            if (msg == null) {
+                continue;
+            }
+            JsonObject obj = new JsonObject(msg);
+            String msgCategory = obj.getJsonObject("meta").getString("category");
+            if (category.equals(msgCategory) && predicate.test(obj)) {
+                return obj;
+            }
+        } while (now < deadline);
         throw new TimeoutException();
     }
 
     @ClientEndpoint
-    private class WebSocketClient {
-        private final LinkedBlockingDeque<String> wsMessages = new LinkedBlockingDeque<>();
+    private static class WebSocketClient {
+        private final LinkedBlockingDeque<String> msgQ = new LinkedBlockingDeque<>();
+        private final Logger logger = Logger.getLogger(getClass());
 
         @OnMessage
         void message(String msg) {
-            wsMessages.add(msg);
+            logger.info(msg);
+            msgQ.add(msg);
         }
     }
 }
