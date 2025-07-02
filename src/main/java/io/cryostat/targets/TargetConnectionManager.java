7290d5383e88bfb6581854efc399e287cb5abfd8
Andrew Azores <aazores@redhat.com>
2025-07-02 10:47:20 -0400

    fixup! fix(discovery): handle k8s IPv6 EndpointSlices

--- src/main/java/io/cryostat/targets/TargetConnectionManager.java
+++ src/main/java/io/cryostat/targets/TargetConnectionManager.java
@@ -353,6 +353,8 @@ public class TargetConnectionManager {
             if (semaphore.isPresent()) {
                 semaphore.get().release();
             }
+            logger.error(e);
+            e.printStackTrace();
             throw e;
         } finally {
             evt.end();
