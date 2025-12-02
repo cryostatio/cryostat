4ebbacd98875902e1c4e622ec64251debabf7167
Andrew Azores <me@andrewazor.es>
2025-12-02 10:20:14 -0500

    fix(rules): archiver job uses metadata labels, not filename pattern, to match recordings (#1179)
    (cherry picked from commit b9730cd94d9ce5d6f592c51e060033b667c014e0)

--- src/test/java/itest/TargetRecordingPatchTest.java
+++ src/test/java/itest/TargetRecordingPatchTest.java
@@ -17,13 +17,14 @@ package itest;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-import io.cryostat.util.HttpStatusCodeIdentifier;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.quarkus.test.junit.QuarkusTest;
 import io.vertx.core.MultiMap;
 import io.vertx.core.buffer.Buffer;
@@ -31,9 +32,9 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpResponse;
 import itest.bases.StandardSelfTest;
-import itest.util.ITestCleanupFailedException;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -60,86 +61,75 @@ public class TargetRecordingPatchTest extends StandardSelfTest {
         return String.format("/api/v4/targets/%d/recordingOptions", getSelfReferenceTargetId());
     }
 
+    @AfterEach
+    void cleanup()
+            throws InterruptedException,
+                    TimeoutException,
+                    ExecutionException,
+                    JsonProcessingException {
+        cleanupSelfActiveAndArchivedRecordings();
+    }
+
     @Test
     void testSaveEmptyRecordingDoesNotArchiveRecordingFile() throws Exception {
         long remoteId = -1;
-        try {
-            MultiMap optionsForm = MultiMap.caseInsensitiveMultiMap();
-            optionsForm.add("toDisk", "false");
-            optionsForm.add("maxSize", "0");
-            HttpResponse<Buffer> optionsResponse =
-                    webClient.extensions().patch(optionsRequestUrl(), null, optionsForm, 5);
-            MatcherAssert.assertThat(optionsResponse.statusCode(), Matchers.equalTo(200));
+        MultiMap optionsForm = MultiMap.caseInsensitiveMultiMap();
+        optionsForm.add("toDisk", "false");
+        optionsForm.add("maxSize", "0");
+        HttpResponse<Buffer> optionsResponse =
+                webClient.extensions().patch(optionsRequestUrl(), null, optionsForm, 5);
+        MatcherAssert.assertThat(optionsResponse.statusCode(), Matchers.equalTo(200));
 
-            // Create an empty recording
-            MultiMap form = MultiMap.caseInsensitiveMultiMap();
-            form.add("recordingName", TEST_RECORDING_NAME);
-            form.add("duration", "5");
-            form.add("events", "template=ALL");
-            HttpResponse<Buffer> postResponse =
-                    webClient.extensions().post(recordingRequestUrl(), form, 5);
-            MatcherAssert.assertThat(postResponse.statusCode(), Matchers.equalTo(201));
-            JsonObject postResult = postResponse.bodyAsJsonObject();
-            remoteId = postResult.getLong("remoteId");
-            // Attempt to save the recording to archive
-            HttpResponse<Buffer> saveResponse =
-                    webClient
-                            .extensions()
-                            .patch(
-                                    String.format("%s/%d", recordingRequestUrl(), remoteId),
-                                    null,
-                                    Buffer.buffer("SAVE"),
-                                    5);
-            MatcherAssert.assertThat(saveResponse.statusCode(), Matchers.equalTo(200));
-            MatcherAssert.assertThat(saveResponse.bodyAsString(), Matchers.any(String.class));
+        // Create an empty recording
+        MultiMap form = MultiMap.caseInsensitiveMultiMap();
+        form.add("recordingName", TEST_RECORDING_NAME);
+        form.add("duration", "5");
+        form.add("events", "template=ALL");
+        HttpResponse<Buffer> postResponse =
+                webClient.extensions().post(recordingRequestUrl(), form, 5);
+        MatcherAssert.assertThat(postResponse.statusCode(), Matchers.equalTo(201));
+        JsonObject postResult = postResponse.bodyAsJsonObject();
+        remoteId = postResult.getLong("remoteId");
+        // Attempt to save the recording to archive
+        HttpResponse<Buffer> saveResponse =
+                webClient
+                        .extensions()
+                        .patch(
+                                String.format("%s/%d", recordingRequestUrl(), remoteId),
+                                null,
+                                Buffer.buffer("SAVE"),
+                                5);
+        MatcherAssert.assertThat(saveResponse.statusCode(), Matchers.equalTo(200));
+        MatcherAssert.assertThat(saveResponse.bodyAsString(), Matchers.any(String.class));
 
-            CountDownLatch latch = new CountDownLatch(1);
-            Future<JsonObject> f =
-                    worker.submit(
-                            () -> {
-                                try {
-                                    return expectNotification(
-                                                    "ArchiveRecordingFailed", 15, TimeUnit.SECONDS)
-                                            .get();
-                                } catch (Exception e) {
-                                    throw new RuntimeException(e);
-                                } finally {
-                                    latch.countDown();
-                                }
-                            });
+        CountDownLatch latch = new CountDownLatch(1);
+        Future<JsonObject> f =
+                worker.submit(
+                        () -> {
+                            try {
+                                return expectNotification(
+                                                "ArchiveRecordingFailed", 15, TimeUnit.SECONDS)
+                                        .get();
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            } finally {
+                                latch.countDown();
+                            }
+                        });
 
-            latch.await(REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+        latch.await(REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS);
 
-            // Assert that no recording was archived
-            CompletableFuture<JsonArray> listRespFuture1 = new CompletableFuture<>();
-            webClient
-                    .get(archivesRequestUrl())
-                    .send(
-                            ar -> {
-                                if (assertRequestStatus(ar, listRespFuture1)) {
-                                    listRespFuture1.complete(ar.result().bodyAsJsonArray());
-                                }
-                            });
-            JsonArray listResp = listRespFuture1.get(REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS);
-            Assertions.assertTrue(listResp.isEmpty());
-
-        } finally {
-            // Clean up recording
-            HttpResponse<Buffer> deleteResponse =
-                    webClient
-                            .extensions()
-                            .delete(
-                                    String.format("%s/%d", deleteRecordingRequestUrl(), remoteId),
-                                    5);
-            if (!HttpStatusCodeIdentifier.isSuccessCode(deleteResponse.statusCode())) {
-                throw new ITestCleanupFailedException();
-            }
-
-            // Reset default target recording options
-            MultiMap optionsForm = MultiMap.caseInsensitiveMultiMap();
-            optionsForm.add("toDisk", "unset");
-            optionsForm.add("maxSize", "unset");
-            webClient.extensions().patch(optionsRequestUrl(), null, optionsForm, 5);
-        }
+        // Assert that no recording was archived
+        CompletableFuture<JsonArray> listRespFuture1 = new CompletableFuture<>();
+        webClient
+                .get(archivesRequestUrl())
+                .send(
+                        ar -> {
+                            if (assertRequestStatus(ar, listRespFuture1)) {
+                                listRespFuture1.complete(ar.result().bodyAsJsonArray());
+                            }
+                        });
+        JsonArray listResp = listRespFuture1.get(REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+        Assertions.assertTrue(listResp.isEmpty());
     }
 }
