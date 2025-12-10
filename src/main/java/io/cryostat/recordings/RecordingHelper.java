1b265a25e119fb63979ce2e39ea719526d53d43e
Andrew Azores <me@andrewazor.es>
2025-12-10 16:13:30 -0500

    fixup! use S3AsyncClient to allow for non-copy direct buffer reads

--- src/main/java/io/cryostat/recordings/RecordingHelper.java
+++ src/main/java/io/cryostat/recordings/RecordingHelper.java
@@ -926,7 +926,7 @@ public class RecordingHelper {
                                                 .partNumber(i)
                                                 .contentLength(Long.valueOf(read))
                                                 .build(),
-                                        AsyncRequestBody.fromRemainingByteBufferUnsafe(buf))
+                                        AsyncRequestBody.fromByteBufferUnsafe(buf))
                                 .get()
                                 .eTag();
                 parts.add(CompletedPart.builder().partNumber(i).eTag(eTag).build());
