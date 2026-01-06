4b96e22f5edefa0ed6b4e5ba1177a91872aa2917
Andrew Azores <me@andrewazor.es>
2026-01-06 13:49:12 -0500

    remove fixme comments

--- src/main/java/io/cryostat/targets/AgentClient.java
+++ src/main/java/io/cryostat/targets/AgentClient.java
@@ -463,7 +463,6 @@ public class AgentClient {
     }
 
     Uni<AsyncProfilerStatus> asyncProfilerStatus() {
-        // FIXME async-profiler interaction should happen via MBean invoke
         return agentRestClient
                 .asyncProfilerStatus()
                 .map(
@@ -477,7 +476,6 @@ public class AgentClient {
     }
 
     Uni<String> dumpAsyncProfile(StartProfileRequest req) {
-        // FIXME async-profiler interaction should happen via MBean invoke
         return agentRestClient
                 .dumpAsyncProfiler(req)
                 .map(
@@ -507,7 +505,6 @@ public class AgentClient {
     }
 
     Uni<List<String>> listAsyncProfiles() {
-        // FIXME async-profiler interaction should happen via MBean invoke
         return agentRestClient
                 .listAsyncProfiler()
                 .map(
@@ -522,7 +519,6 @@ public class AgentClient {
     }
 
     Uni<Boolean> deleteAsyncProfile(String id) {
-        // FIXME async-profiler interaction should happen via MBean invoke
         return agentRestClient
                 .deleteAsyncProfiler(id)
                 .map(
@@ -531,7 +527,6 @@ public class AgentClient {
     }
 
     Uni<InputStream> streamAsyncProfile(String id) {
-        // FIXME async-profiler interaction should happen via MBean invoke
         return agentRestClient
                 .streamAsyncProfile(id)
                 .map(
