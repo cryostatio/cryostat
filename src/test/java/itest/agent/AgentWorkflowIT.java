2f19fbdd9bd5a6c2adb8606aa48abe274c4909a9
Andrew Azores <me@andrewazor.es>
2026-02-06 13:12:42 -0500

    enable agent tests unconditionally

--- src/test/java/itest/agent/AgentWorkflowIT.java
+++ src/test/java/itest/agent/AgentWorkflowIT.java
@@ -28,16 +28,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 @QuarkusIntegrationTest
 @TestMethodOrder(OrderAnnotation.class)
-@EnabledIfEnvironmentVariable(
-        named = "PR_CI",
-        matches = "true",
-        disabledReason =
-                "Runs well in PR CI under Docker, but not on main CI or locally under Podman due to"
-                        + " testcontainers 'Broken Pipe' IOException")
 @WithTestResource(
         value = AgentApplicationResource.class,
         scope = TestResourceScope.MATCHING_RESOURCES)
