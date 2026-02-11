6c24585b93749ae449b14f976dce659cc27eb9b5
Andrew Azores <aazores@ibm.com>
2026-02-11 12:58:33 -0500

    eliminate mostly unused StandardSelfTest

--- src/test/java/itest/CryostatTemplateIT.java
+++ src/test/java/itest/CryostatTemplateIT.java
@@ -18,8 +18,10 @@ package itest;
 import static io.restassured.RestAssured.given;
 
 import java.io.File;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 import org.openjdk.jmc.flightrecorder.configuration.events.EventConfiguration;
 import org.openjdk.jmc.flightrecorder.configuration.model.xml.XMLAttributeInstance;
@@ -27,15 +29,22 @@ import org.openjdk.jmc.flightrecorder.configuration.model.xml.XMLModel;
 import org.openjdk.jmc.flightrecorder.configuration.model.xml.XMLTagInstance;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import itest.bases.StandardSelfTest;
+import io.vertx.core.json.JsonObject;
+import itest.bases.WebSocketTestBase;
+import org.apache.commons.lang3.StringUtils;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @QuarkusIntegrationTest
-public class CryostatTemplateIT extends StandardSelfTest {
+public class CryostatTemplateIT extends WebSocketTestBase {
+
+    private static final String SELF_JMX_URL = "service:jmx:rmi:///jndi/rmi://localhost:0/jmxrmi";
+    private static final String SELFTEST_ALIAS = "selftest";
+    private static volatile String selfCustomTargetLocation;
 
     @Test
     public void shouldHaveCryostatTemplate() throws Exception {
@@ -75,5 +84,50 @@ public class CryostatTemplateIT extends StandardSelfTest {
         Assertions.assertNotNull(labelAttr, "Label attribute should not be null");
         String templateName = labelAttr.getExplicitValue();
         MatcherAssert.assertThat(templateName, Matchers.equalTo("Cryostat"));
+    }
+
+    private static long getSelfReferenceTargetId() {
+        tryDefineSelfCustomTarget();
+        String path = URI.create(selfCustomTargetLocation).getPath();
+        Response response = given().when().get(path).then().statusCode(200).extract().response();
+        JsonObject body = new JsonObject(response.body().asString());
+        return body.getLong("id");
+    }
+
+    private static void tryDefineSelfCustomTarget() {
+        if (selfCustomTargetExists()) {
+            return;
+        }
+        JsonObject self =
+                new JsonObject(Map.of("connectUrl", SELF_JMX_URL, "alias", SELFTEST_ALIAS));
+        Response response =
+                given().contentType(ContentType.JSON)
+                        .body(self.encode())
+                        .when()
+                        .post("/api/v4/targets")
+                        .then()
+                        .extract()
+                        .response();
+        if (response.statusCode() < 200 || response.statusCode() >= 300) {
+            throw new IllegalStateException(String.format("HTTP %d", response.statusCode()));
+        }
+        selfCustomTargetLocation = URI.create(response.header("Location")).getPath();
+    }
+
+    private static boolean selfCustomTargetExists() {
+        if (StringUtils.isBlank(selfCustomTargetLocation)) {
+            return false;
+        }
+        try {
+            Response response = given().when().get(selfCustomTargetLocation);
+            boolean result = response.statusCode() >= 200 && response.statusCode() < 300;
+            if (!result) {
+                selfCustomTargetLocation = null;
+            }
+            return result;
+        } catch (Exception e) {
+            selfCustomTargetLocation = null;
+            return false;
+        }
     }
 }
