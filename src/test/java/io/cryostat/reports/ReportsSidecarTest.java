19edaf53819c5dcbb226c09f24e6b982e60ac05d
Andrew Azores <aazores@ibm.com>
2026-02-12 17:14:42 -0500

    add test for sidecar report generation

---
+++ src/test/java/io/cryostat/reports/ReportsSidecarTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright The Cryostat Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.cryostat.reports;
+
+import io.cryostat.resources.ReportsSidecarResource;
+import io.cryostat.resources.S3StorageResource;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+@TestHTTPEndpoint(Reports.class)
+@QuarkusTestResource(value = S3StorageResource.class, restrictToAnnotatedClass = true)
+@QuarkusTestResource(value = ReportsSidecarResource.class, restrictToAnnotatedClass = true)
+public class ReportsSidecarTest extends ReportsTest {}
