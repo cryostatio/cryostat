c11e204aec238597f9b9e089cf706595eafcc125
Andrew Azores <aazores@ibm.com>
2026-05-22 09:51:19 -0400

    fixup! more robust checks

--- src/test/java/io/cryostat/rules/RulesArchiverTest.java
+++ src/test/java/io/cryostat/rules/RulesArchiverTest.java
@@ -22,7 +22,6 @@ import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import io.cryostat.AbstractTransactionalTestBase;
