andrewazores/cryostat3

feat(archive): include recording labels for startTime, duration, activeRecordingId

src/main/java/io/cryostat/recordings/ArchivedRecordings.java
@@ -165,6 +165,12 @@ public class ArchivedRecordings {
             rawLabels.getMap().forEach((k, v) -> labels.put(k, v.toString()));
         }
         labels.put("jvmId", jvmId);
+        resolveActiveRecordingId(jvmId, labels)
+                .ifPresent(
+                        id ->
+                                labels.put(
+                                        RecordingHelper.ACTIVE_RECORDING_ID_LABEL,
+                                        String.valueOf(id)));
         Metadata metadata = new Metadata(labels);
         logger.tracev(
                 "recording:{0}, labels:{1}, maxFiles:{2}", recording.fileName(), labels, maxFiles);
@@ -189,6 +195,42 @@ public class ArchivedRecordings {
                         logger.error(ioe);
                     }
                 });
+    }
+
+    private Optional<Long> resolveActiveRecordingId(String jvmId, Map<String, String> labels) {
+        String sourceRecordingId = labels.get(RecordingHelper.SOURCE_RECORDING_ID_LABEL);
+        if (StringUtils.isBlank(sourceRecordingId)) {
+            logger.warnv(
+                    "No {0} provided for archived recording upload on target {1}",
+                    RecordingHelper.SOURCE_RECORDING_ID_LABEL, jvmId);
+            return Optional.empty();
+        }
+        long remoteId;
+        try {
+            remoteId = Long.parseLong(sourceRecordingId);
+        } catch (NumberFormatException e) {
+            logger.warnv(
+                    e,
+                    "Invalid {0}={1} provided for archived recording upload on target {2}",
+                    RecordingHelper.SOURCE_RECORDING_ID_LABEL,
+                    sourceRecordingId,
+                    jvmId);
+            return Optional.empty();
+        }
+        Optional<Target> target = Target.getTargetByJvmId(jvmId);
+        if (target.isEmpty()) {
+            logger.warnv("No target found for archived recording upload on target {0}", jvmId);
+            return Optional.empty();
+        }
+        Optional<ActiveRecording> recording =
+                recordingHelper.getActiveRecording(target.get(), remoteId);
+        if (recording.isEmpty()) {
+            logger.warnv(
+                    "No active recording found for {0}={1} on target {2}",
+                    RecordingHelper.SOURCE_RECORDING_ID_LABEL, sourceRecordingId, jvmId);
+            return Optional.empty();
+        }
+        return Optional.of(recording.get().id);
     }
 
     @GET

cryostatio/cryostat

src/main/java/io/cryostat/recordings/ArchivedRecordings.java
@@ -165,6 +165,12 @@ public class ArchivedRecordings {
             rawLabels.getMap().forEach((k, v) -> labels.put(k, v.toString()));
         }
         labels.put("jvmId", id);
+        resolveActiveRecordingId(id, labels)
+                .ifPresent(
+                        i ->
+                                labels.put(
+                                        RecordingHelper.ACTIVE_RECORDING_ID_LABEL,
+                                        String.valueOf(i)));
         Metadata metadata = new Metadata(labels);
         logger.tracev(
                 "recording:{0}, labels:{1}, maxFiles:{2}", recording.fileName(), labels, maxFiles);
@@ -202,6 +208,42 @@ public class ArchivedRecordings {
                     });
         }
         doUpload(recording, metadata, id);
+    }
+
+    private Optional<Long> resolveActiveRecordingId(String jvmId, Map<String, String> labels) {
+        String sourceRecordingId = labels.get(RecordingHelper.SOURCE_RECORDING_ID_LABEL);
+        if (StringUtils.isBlank(sourceRecordingId)) {
+            logger.warnv(
+                    "No {0} provided for archived recording upload on target {1}",
+                    RecordingHelper.SOURCE_RECORDING_ID_LABEL, jvmId);
+            return Optional.empty();
+        }
+        long remoteId;
+        try {
+            remoteId = Long.parseLong(sourceRecordingId);
+        } catch (NumberFormatException e) {
+            logger.warnv(
+                    e,
+                    "Invalid {0}={1} provided for archived recording upload on target {2}",
+                    RecordingHelper.SOURCE_RECORDING_ID_LABEL,
+                    sourceRecordingId,
+                    jvmId);
+            return Optional.empty();
+        }
+        Optional<Target> target = Target.getTargetByJvmId(jvmId);
+        if (target.isEmpty()) {
+            logger.warnv("No target found for archived recording upload on target {0}", jvmId);
+            return Optional.empty();
+        }
+        Optional<ActiveRecording> recording =
+                recordingHelper.getActiveRecording(target.get(), remoteId);
+        if (recording.isEmpty()) {
+            logger.warnv(
+                    "No active recording found for {0}={1} on target {2}",
+                    RecordingHelper.SOURCE_RECORDING_ID_LABEL, sourceRecordingId, jvmId);
+            return Optional.empty();
+        }
+        return Optional.of(recording.get().id);
     }
 
     @GET