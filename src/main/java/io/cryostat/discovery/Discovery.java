a7b475a38b276c9e7c1d475d8236a3ece510f3f4
Andrew Azores <aazores@ibm.com>
2026-08-21 17:01:25 -0400

    restore callback validation, but skip for proxy-verified requests

--- src/main/java/io/cryostat/discovery/Discovery.java
+++ src/main/java/io/cryostat/discovery/Discovery.java
@@ -888,7 +888,26 @@ public class Discovery {
                             ConfigProperties.AGENT_TLS_REQUIRED));
         }
 
-        return new CallbackValidation(callbackUri, unauthCallback, remoteAddress);
+        if (RbacHttpAuthenticationMechanism.isAgentProxyRequest(ctx)) {
+            return new CallbackValidation(callbackUri, unauthCallback, remoteAddress);
+        }
+
+        try {
+            for (InetAddress callbackAddress : InetAddress.getAllByName(callbackUri.getHost())) {
+                if (remoteAddress.equals(callbackAddress)) {
+                    return new CallbackValidation(callbackUri, unauthCallback, remoteAddress);
+                }
+            }
+        } catch (UnknownHostException e) {
+            throw new BadRequestException(
+                    String.format("%s host could not be resolved: %s", parameterName, callbackUri),
+                    e);
+        }
+
+        throw new BadRequestException(
+                String.format(
+                        "%s host does not resolve to the client address %s: %s",
+                        parameterName, remoteAddress.getHostAddress(), callbackUri));
     }
 
     private DiscoveryPlugin findOrCreatePlugin(
@@ -1460,11 +1479,6 @@ public class Discovery {
         InetAddress addr = null;
         if (ctx.request() != null && ctx.request().remoteAddress() != null) {
             addr = jwtValidator.tryResolveAddress(addr, ctx.request().remoteAddress().host());
-        }
-        if (ctx.request() != null && ctx.request().headers() != null) {
-            addr =
-                    jwtValidator.tryResolveAddress(
-                            addr, ctx.request().headers().get(X_FORWARDED_FOR));
         }
         return addr;
     }
